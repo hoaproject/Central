7dc42d52bdf3677b74c9efd322110a86aef160d5
hywan <devnull@localhost>
2008-12-17 23:02:18 +0000

    New repository layout (again).

---
+++ Framework/Graph/AdjacencyList.php
@@ -0,0 +1,360 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Graph
+ * @subpackage  Hoa_Graph_AdjacencyList
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Graph
+ */
+import('Graph.~');
+
+/**
+ * Hoa_Graph_Exception
+ */
+import('Graph.Exception');
+
+/**
+ * Hoa_Graph_Node_Interface
+ */
+import('Graph.Node.Interface');
+
+/**
+ * Class Hoa_Graph_AdjacencyList.
+ *
+ * Code an adjacency list graph.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Graph
+ * @subpackage  Hoa_Graph_AdjacencyList
+ */
+
+class Hoa_Graph_AdjacencyList extends Hoa_Graph {
+
+    /**
+     * Node value index.
+     *
+     * @const int
+     */
+    const NODE_VALUE      = 0;
+    
+    /**
+     * Node child index.
+     *
+     * @const int
+     */
+    const NODE_CHILD      = 1;
+
+
+
+    /**
+     * Propagate the construction.
+     *
+     * @access  public
+     * @param   bool    $loop    Allow or not loop.
+     * @return  void
+     */
+    public function __construct ( $loop = parent::DISALLOW_LOOP ) {
+
+        parent::__construct($loop);
+    }
+
+    /**
+     * Disallow to get a new instance.
+     *
+     * @access  public
+     * @param   string  $type    Type of graph needed.
+     * @return  void
+     * @throw   Hoa_Graph_Exception
+     */
+    public static function getInstance ( $type = parent::TYPE_ADJACENCYLIST ) {
+
+        throw new Hoa_Graph_Exception(
+            'Cannot get a new from a typped graph.', 0);
+    }
+
+    /**
+     * Add a node.
+     *
+     * @access  public
+     * @param   Hoa_Graph_Node_Interface  $node      Node to add.
+     * @param   mixed                     $parent    Parent of node.
+     * @return  void
+     * @throw   Hoa_Graph_Exception
+     */
+    public function addNode ( Hoa_Graph_Node_Interface $node,
+                              $parent = array() ) {
+
+        if(!is_array($parent))
+            $parent = array($parent);
+
+        if(parent::DISALLOW_LOOP === $this->isLoopAllow()) {
+
+            if(true === $this->nodeExists($node->getNodeId()))
+                throw new Hoa_Graph_Exception(
+                    'Node %s already exists.', 1, $node->getNodeId());
+
+            if(in_array($node->getNodeId(), $parent))
+                throw new Hoa_Graph_Exception(
+                    'Node %s cannot define itself in parent.', 2,
+                    $node->getNodeId());
+        }
+
+        $this->nodes[$node->getNodeId()] = array(
+            self::NODE_VALUE => $node,
+            self::NODE_CHILD => array()
+        );
+
+        foreach($parent as $foo => $nodeId) {
+
+            if($nodeId instanceof Hoa_Graph_Node_Interface)
+                $nodeId = $nodeId->getNodeId();
+
+            if(parent::DISALLOW_LOOP === $this->isLoopAllow())
+                if(false === $this->nodeExists($nodeId))
+                    throw new Hoa_Graph_Exception(
+                        'Node %s does not exist.', 3, $nodeId);
+
+            $this->nodes[$nodeId][self::NODE_CHILD][] = $node->getNodeId();
+        }
+    }
+
+    /**
+     * Check if a node does already exist or not.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  bool
+     */
+    public function nodeExists ( $nodeId ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        return isset($this->nodes[$nodeId]);
+    }
+
+    /**
+     * Get a node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  object
+     * @throw   Hoa_Graph_Exception
+     */
+    public function getNode ( $nodeId ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        if(false === $this->nodeExists($nodeId))
+            throw new Hoa_Graph_Exception(
+                'Node %s does not exist.', 4, $nodeId);
+
+        return $this->nodes[$nodeId][self::NODE_VALUE];
+    }
+
+    /**
+     * Get parent of a specific node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  object
+     * @throw   Hoa_Graph_Exception
+     */
+    public function getParent ( $nodeId ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        if(false === $this->nodeExists($nodeId))
+            throw new Hoa_Graph_Exception(
+                'Node %s does not exist.', 5, $nodeId);
+
+        $parent = new ArrayObject(
+            array(), ArrayObject::ARRAY_AS_PROPS, 'ArrayIterator');
+
+        foreach($this->getNodes() as $id => $values ) {
+
+            if(parent::DISALLOW_LOOP === $this->isLoopAllow())
+                if($nodeId == $id)
+                    continue;
+
+            if(in_array($nodeId, $values[self::NODE_CHILD]))
+                $parent->offsetSet(
+                    $id,
+                    $this->getNode($id)
+                );
+        }
+
+        return $parent;
+    }
+
+    /**
+     * Get child of a specific node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  object
+     * @throw   Hoa_Graph_Exception
+     */
+    public function getChild ( $nodeId ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        if(false === $this->nodeExists($nodeId))
+            throw new Hoa_Graph_Exception(
+                'Node %s does not exist.', 6, $nodeId);
+
+        $child = new ArrayObject(
+            array(), ArrayObject::ARRAY_AS_PROPS, 'ArrayIterator');
+
+        foreach($this->nodes[$nodeId][self::NODE_CHILD] as $foo => $id)
+            $child->offsetSet(
+                $id,
+                $this->getNode($id)
+            );
+
+        return $child;
+    }
+
+    /**
+     * Delete a node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId       The node ID or the node instance.
+     * @param   bool    $propagate    Propagate the erasure.
+     * @return  void
+     * @throw   Hoa_Graph_Exception
+     */
+    public function deleteNode ( $nodeId, $propagate = parent::DELETE_RESTRICT ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        if(false === $this->nodeExists($nodeId))
+            return;
+
+        if($propagate == parent::DELETE_RESTRICT)
+
+            if(empty($this->nodes[$nodeId][self::NODE_CHILD])) {
+
+                unset($this->nodes[$nodeId]);
+
+                foreach($this->getNodes() as $id => $values )
+                    if(false !== $key = array_search($nodeId, $values[self::NODE_CHILD]))
+                        unset($this->nodes[$id][self::NODE_CHILD][$key]);
+            }
+            else
+                throw new Hoa_Graph_Exception(
+                    'Cannot delete %s node in restrict delete mode, because ' .
+                    'it has one or more children.', 7, $nodeId);
+        else {
+
+            foreach($this->nodes[$nodeId][self::NODE_CHILD] as $foo => $id)
+                $this->deleteNode($id, $propagate);
+
+            $this->deleteNode($nodeId, parent::DELETE_RESTRICT);
+        }
+    }
+
+    /**
+     * Whether node is a leaf, i.e. does not have any child.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  bool
+     * @throw   Hoa_Graph_Exception
+     */
+    public function isLeaf ( $nodeId ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        if(false === $this->nodeExists($nodeId))
+            throw new Hoa_Graph_Exception(
+                'Node %s does not exist.', 8, $nodeId);
+
+        return empty($this->nodes[$nodeId][self::NODE_CHILD]);
+    }
+
+    /**
+     * Whether node is a root, i.e. does not have any parent.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  bool
+     * @throw   Hoa_Graph_Exception
+     */
+    public function isRoot ( $nodeId ) {
+
+        if($nodeId instanceof Hoa_Graph_Node_Interface)
+            $nodeId = $nodeId->getNodeId();
+
+        if(false === $this->nodeExists($nodeId))
+            throw new Hoa_Graph_Exception(
+                'Node %s does not exist.', 9, $nodeId);
+
+        return count($this->getParent($nodeId)) == 0;
+    }
+
+    /**
+     * Print the graph in the DOT language.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function __toString ( ) {
+
+        $out = 'digraph {' . "\n";
+
+        foreach($this->getNodes() as $nodeId => $foo)
+            $out .= '    ' . $nodeId . ';' . "\n";
+
+        foreach($this->getNodes() as $nodeId => $foo)
+            foreach($this->getChild($nodeId) as $fooo => $child)
+                $out .= '    ' . $nodeId . ' -> ' . $child->getNodeId() . ';' .  "\n";
+
+        $out .= '}';
+
+        return $out;
+    }
+}
---
+++ Framework/Graph/Graph.php
@@ -0,0 +1,289 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Graph
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Graph_Exception
+ */
+import('Graph.Exception');
+
+/**
+ * Hoa_Factory
+ */
+import('Factory.~');
+
+/**
+ * Class Hoa_Graph.
+ *
+ * Get instance of different graph type.
+ * When getting an instance of a type of a graph, the graph type (e.g.
+ * Hoa_Graph_AdjacencyList) extends this class. It is like an abstract
+ * factory …
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Graph
+ */
+
+abstract class Hoa_Graph {
+
+    /**
+     * Graph type.
+     *
+     * @const string
+     */
+    const TYPE_ADJACENCYLIST = 'AdjacencyList';
+
+    /**
+     * Allow loop when building graph (when adding nodes).
+     *
+     * @const bool
+     */
+    const ALLOW_LOOP      = true;
+
+    /**
+     * Disallow loop when building graph (when adding nodes).
+     *
+     * @const bool
+     */
+    const DISALLOW_LOOP   = false;
+
+    /**
+     * Propagate delete.
+     *
+     * @const bool
+     */
+    const DELETE_CASCADE = true;
+
+    /**
+     * Restrict delete.
+     *
+     * @const bool
+     */
+    const DELETE_RESTRICT = false;
+
+    /**
+     * All nodes.
+     *
+     * @var Hoa_Graph array
+     */
+    protected $nodes = array();
+
+    /**
+     * If allow loop when building graph, it is set to ALLOW_LOOP (true),
+     * DISALLOW_LOOP (false) else.
+     *
+     * @var Hoa_Graph bool
+     */
+    protected $loop = self::DISALLOW_LOOP;
+
+
+
+    /**
+     * Get an empty graph.
+     *
+     * @access  protected
+     * @param   bool       $loop    Allow or not loop.
+     * @return  void
+     */
+    protected function __construct ( $loop = self::DISALLOW_LOOP ) {
+
+        $this->allowLoop($loop);
+    }
+
+    /**
+     * Make an instance of a specific graph.
+     *
+     * @access  public
+     * @param   string  $type    Type of graph needed.
+     * @return  void
+     * @throw   Hoa_Graph_Exception
+     */
+    public static function getInstance ( $type = self::TYPE_ADJACENCYLIST ) {
+
+        if($type != self::TYPE_ADJACENCYLIST)
+            throw new Hoa_Graph_Exception(
+                'Type %s is not supported. Only self:TYPE_ADJACENCYLIST is ' .
+                'supported.', 0, $type);
+
+        $args = func_get_args();
+        array_shift($args);
+
+        $handler = null;
+
+        try {
+
+            $handler = Hoa_Factory::get('Graph', $type, $args);
+        }
+        catch ( Hoa_Factory_Exception $e ) {
+
+            throw new Hoa_Graph_Exception($e->getMessage(), $e->getCode());
+        }
+
+        return $handler;
+    }
+
+    /**
+     * Add a node.
+     *
+     * @access  public
+     * @param   Hoa_Graph_Node_Interface  $node      Node to add.
+     * @param   mixed                     $parent    Parent of node.
+     * @return  void
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function addNode ( Hoa_Graph_Node_Interface $node,
+                                       $parent = array() );
+
+    /**
+     * Check if a node does already exist or not.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  bool
+     */
+    abstract public function nodeExists ( $nodeId );
+
+    /**
+     * Get a node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  object
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function getNode ( $nodeId );
+
+    /**
+     * Get all nodes.
+     *
+     * @access  protected
+     * @return  array
+     */
+    protected function getNodes ( ) {
+
+        return $this->nodes;
+    }
+
+    /**
+     * Get parent of a specific node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  object
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function getParent ( $nodeId );
+
+    /**
+     * Get child of a specific node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  object
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function getChild ( $nodeId );
+
+    /**
+     * Delete a node.
+     *
+     * @access  public
+     * @param   mixed   $nodeId       The node ID or the node instance.
+     * @param   bool    $propagate    Propagate the erasure.
+     * @return  void
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function deleteNode ( $nodeId, $propagate = self::DELETE_RESTRICT );
+
+    /**
+     * Whether node is a leaf, i.e. does not have any child.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  bool
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function isLeaf ( $nodeId );
+
+    /**
+     * Whether node is a root, i.e. does not have any parent.
+     *
+     * @access  public
+     * @param   mixed   $nodeId    The node ID or the node instance.
+     * @return  bool
+     * @throw   Hoa_Graph_Exception
+     */
+    abstract public function isRoot ( $nodeId );
+
+    /**
+     * Set the loop mode (self::ALLOW_LOOP or self::DISALLOW_LOOP).
+     *
+     * @access  public
+     * @param   bool    $loop    Allow or not loop.
+     * @return  bool
+     */
+    public function allowLoop ( $loop = self::DISALLOW_LOOP ) {
+
+        $old        = $this->loop;
+        $this->loop = $loop;
+
+        return $old;
+    }
+
+    /**
+     * Get the loop mode.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isLoopAllow ( ) {
+
+        return $this->loop;
+    }
+
+    /**
+     * Print the graph in the DOT language.
+     *
+     * @access  public
+     * @return  string
+     */
+    abstract public function __toString ( );
+}
