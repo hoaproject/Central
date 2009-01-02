914b7a1c493333c9c9ed8126583fa23110c1047b
hywan <devnull@localhost>
2009-01-02 17:18:55 +0000

    Write the whitespace structure (it's a pretext for testing SVN hooks).

---
+++ Framework/Tokenizer/Token/Whitespace.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Tokenizer
+ * @subpackage  Hoa_Tokenizer_Token_Whitespace
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Tokenizer_Token_Util_Exception
+ */
+import('Tokenizer.Token.Util.Exception');
+
+/**
+ * Hoa_Tokenizer_Token_Util_Interface
+ */
+import('Tokenizer.Token.Util.Interface');
+
+/**
+ * Hoa_Tokenizer
+ */
+import('Tokenizer.~');
+
+/**
+ * Class Hoa_Tokenizer_Token_Whitespace.
+ *
+ * Represent a whitespace.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Tokenizer
+ * @subpackage  Hoa_Tokenizer_Token_Whitespace
+ */
+
+class Hoa_Tokenizer_Token_Whitespace implements Hoa_Tokenizer_Token_Util_Interface {
+
+    /**
+     * Transform token to “tokenizer array”.
+     *
+     * @access  public
+     * @param   int     $context    Context.
+     * @return  array
+     */
+    public function toArray ( $context = Hoa_Tokenizer::CONTEXT_STANDARD ) {
+
+        return array(array(
+            
+        ));
+    }
+}
