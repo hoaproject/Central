aebfdf1feecf1ffc100e7db0c029c33208abf4da
hywan <devnull@localhost>
2009-01-09 19:45:10 +0000

    Write the exception class (“throw” token).

---
+++ Framework/Tokenizer/Token/Exception.php
@@ -0,0 +1,153 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Tokenizer
+ * @subpackage  Hoa_Tokenizer_Token_Exception
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Tokenizer_Token_Util_Exception
+ */
+import('Tokenizer.Token.Util.Exception');
+
+/**
+ * Hoa_Tokenizer_Token_Util_Interface_Tokenizable
+ */
+import('Tokenizer.Token.Util.Interface.Tokenizable');
+
+/**
+ * Hoa_Tokenizer
+ */
+import('Tokenizer.~');
+
+/**
+ * Class Hoa_Tokenizer_Token_Exception.
+ *
+ * .
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Tokenizer
+ * @subpackage  Hoa_Tokenizer_Token_Exception
+ */
+
+class Hoa_Tokenizer_Token_Exception implements Hoa_Tokenizer_Token_Util_Interface_Tokenizable {
+
+    /**
+     * Exception wrapper.
+     *
+     * @var Hoa_Tokenizer_Token_Exception mixed
+     */
+    protected $_exception = null;
+
+
+
+    /**
+     * Constructor.
+     *
+     * @access  public
+     * @param   mixed   $exception    Exception.
+     * @return  void
+     */
+    public function __construct ( $exception ) {
+
+        $this->setException($exception);
+
+        return;
+    }
+
+    /**
+     * Set exception.
+     *
+     * @access  public
+     * @param   mixed   $exception    Exception.
+     * @return  mixed
+     */
+    public function setException  ( $exception ) {
+
+        switch(get_class($exception)) {
+
+            case 'Hoa_Tokenizer_Token_New':
+            case 'Hoa_Tokenizer_Token_Variable':
+            case 'Hoa_Tokenizer_Token_Call_Attribute':
+            case 'Hoa_Tokenizer_Token_Call_Function':
+            case 'Hoa_Tokenizer_Token_Call_Method':
+            case 'Hoa_Tokenizer_Token_Call_StaticAttribute':
+            case 'Hoa_Tokenizer_Token_Call_StaticMethod':
+              break;
+
+            default:
+                throw new Hoa_Tokenizer_Token_Util_Exception(
+                    'Cannot throw a class that represents a %s.', 0,
+                    get_class($exception));
+        }
+
+        $this->_exception = $exception;
+
+        return $this->_exception;
+    }
+
+    /**
+     * Get exception.
+     *
+     * @access  public
+     * @return  mixed
+     */
+    public function getException ( ) {
+
+        return $this->_exception;
+    }
+
+    /**
+     * Transform token to “tokenizer array”.
+     *
+     * @access  public
+     * @return  array
+     */
+    public function tokenize ( ) {
+
+        return array_merge(
+            array(array(
+                0 => Hoa_Tokenizer::_THROW,
+                1 => 'throw',
+                2 => -1
+            )),
+            $this->getException()->tokenize()
+        );
+    }
+}
