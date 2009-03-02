9988dbe843180040c922525c9104e0b46c785b65
hywan <devnull@localhost>
2009-03-02 00:30:34 +0000

    Add visitors for the Php token.

---
+++ Framework/Pom/Token/Util/Visitor/PrettyPrint/Php.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Pom
+ * @subpackage  Hoa_Pom_Token_Util_Visitor_PrettyPrint_Php
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Pom_Token_Util_Exception
+ */
+import('Pom.Token.Util.Exception');
+
+/**
+ * Hoa_Pom
+ */
+import('Pom.~');
+
+/**
+ * Hoa_Pom_Token_Php
+ */
+import('Pom.Token.Php');
+
+/**
+ * Hoa_Pom_Token_Util_Visitor_PrettyPrint_Aggregate
+ */
+import('Pom.Token.Util.Visitor.PrettyPrint.Aggregate');
+
+/**
+ * Class Hoa_Pom_Token_Util_Visitor_PrettyPrint_Php.
+ *
+ * Visit an open or close tag.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Pom
+ * @subpackage  Hoa_Pom_Token_Util_Visitor_PrettyPrint_Php
+ */
+
+class Hoa_Pom_Token_Util_Visitor_PrettyPrint_Php extends Hoa_Pom_Token_Util_Visitor_PrettyPrint_Aggregate {
+
+    /**
+     * Visit an open or close tag.
+     *
+     * @access  public
+	 * @param   Hoa_Visitor_Element  $element    Element to visit.
+	 * @param   mixed                $handle     Handle (reference).
+     * @return  string
+     */
+    public function visitPhp ( Hoa_Visitor_Element $element, &$handle = null ) {
+
+        $out = $element->getTag();
+
+        switch($element->getType()) {
+
+            case Hoa_Pom::_OPEN_TAG:
+                $out .= "\n\n";
+              break;
+
+            case Hoa_Pom::_CLOSE_TAG:
+                $out  = "\n\n" . $out;
+              break;
+        }
+
+        return $out;
+    }
+}
