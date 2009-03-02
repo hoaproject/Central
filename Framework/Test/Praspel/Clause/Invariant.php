67070491d65976c4aaff32d1fb1eae2addebb0f2
hywan <devnull@localhost>
2009-03-02 00:51:34 +0000

    Write the “invariant” clause. The only restriction is that a free-variable cannot be a constructor (like \result or \old(e)).

---
+++ Framework/Test/Praspel/Clause/Invariant.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Test
+ * @subpackage  Hoa_Test_Praspel_Clause_Invariant
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Test_Praspel_Exception
+ */
+import('Test.Praspel.Exception');
+
+/**
+ * Hoa_Test_Praspel_Clause
+ */
+import('Test.Praspel.Clause');
+
+/**
+ * Hoa_Test_Praspel_Clause_Contract
+ */
+import('Test.Praspel.Clause.Contract');
+
+/**
+ * Class Hoa_Test_Praspel_Clause_Invariant.
+ *
+ * .
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Test
+ * @subpackage  Hoa_Test_Praspel_Clause_Invariant
+ */
+
+class Hoa_Test_Praspel_Clause_Invariant extends    Hoa_Test_Praspel_Clause_Contract
+                                        implements Hoa_Test_Praspel_Clause {
+
+    /**
+     * Declare a free variable.
+     *
+     * @access  public
+     * @param   string  $name    Free variable name.
+     * @return  Hoa_Test_Praspel_FreeVariable
+     * @throws  Hoa_Test_Praspel_Exception
+     */
+    public function declareFreeVariable ( $name ) {
+
+        if($name[0] == '\\')
+            throw new Hoa_Test_Praspel_Exception(
+                'Constructors are not allowed in “ensures” clause, given %s.',
+                0, $name);
+
+        parent::declareFreeVariable($name);
+    }
+}
