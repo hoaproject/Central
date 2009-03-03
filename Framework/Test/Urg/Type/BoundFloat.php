2ed9b6a07800fb0d1d59de93f808360758fa12b7
hywan <devnull@localhost>
2009-03-03 02:37:06 +0000

    Correct bugs with bound (lower or upper, close or open etc.). Rewrite code to be more standard in the “bound logic”.

--- Framework/Test/Urg/Type/BoundFloat.php
+++ Framework/Test/Urg/Type/BoundFloat.php
@@ -71,18 +71,18 @@ class Hoa_Test_Urg_Type_BoundFloat extends    Hoa_Test_Urg_Type_Float
                                    implements Hoa_Test_Urg_Type_Interface_Type {
 
     /**
-     * Upper bound statement (given by parent::BOUND_* constants).
-     *
-     * @var Hoa_Test_Urg_Type_BoundFloat bool
-     */
-    protected $_upperBoundStatement = parent::BOUND_CLOSE;
-
-    /**
      * Lower bound statement (given by parent::BOUND_* constants).
      *
      * @var Hoa_Test_Urg_Type_BoundFloat bool
      */
     protected $_lowerBoundStatement = parent::BOUND_CLOSE;
+
+    /**
+     * Upper bound statement (given by parent::BOUND_* constants).
+     *
+     * @var Hoa_Test_Urg_Type_BoundFloat bool
+     */
+    protected $_upperBoundStatement = parent::BOUND_CLOSE;
 
 
 
@@ -92,13 +92,13 @@ class Hoa_Test_Urg_Type_BoundFloat extends    Hoa_Test_Urg_Type_Float
      * @access  public
      * @param   int     $lowerValue        Lower bound value.
      * @param   int     $upperValue        Upper bound value.
-     * @param   bool    $upperStatement    Given by constant parent::BOUND_*.
      * @param   bool    $lowerStatement    Given by constant parent::BOUND_*.
+     * @param   bool    $upperStatement    Given by constant parent::BOUND_*.
      * @return  void
      */
     public function __construct ( $lowerValue, $upperValue,
-                                  $upperStatement = parent::BOUND_CLOSE,
-                                  $lowerStatement = parent::BOUND_CLOSE ) {
+                                  $lowerStatement = parent::BOUND_CLOSE,
+                                  $upperStatement = parent::BOUND_CLOSE ) {
 
         if($lower > $upper) {
 
@@ -154,21 +154,6 @@ class Hoa_Test_Urg_Type_BoundFloat extends    Hoa_Test_Urg_Type_Float
     }
 
     /**
-     * Set upper bound statement.
-     *
-     * @access  protected
-     * @param   bool       $upperStatement    Given by constant parent::BOUND_*.
-     * @return  bool
-     */
-    protected function setUpperBoundStatement ( $upperStatement ) {
-
-        $old                        = $this->_upperBoundStatement;
-        $this->_upperBoundStatement = $upperStatement;
-
-        return $old;
-    }
-
-    /**
      * Set lower bound statement.
      *
      * @access  protected
@@ -184,14 +169,18 @@ class Hoa_Test_Urg_Type_BoundFloat extends    Hoa_Test_Urg_Type_Float
     }
 
     /**
-     * Get upper bound statement.
+     * Set upper bound statement.
      *
      * @access  protected
+     * @param   bool       $upperStatement    Given by constant parent::BOUND_*.
      * @return  bool
      */
-    protected function getUpperBoundStatement ( ) {
+    protected function setUpperBoundStatement ( $upperStatement ) {
 
-        return $this->_upperBoundStatement;
+        $old                        = $this->_upperBoundStatement;
+        $this->_upperBoundStatement = $upperStatement;
+
+        return $old;
     }
 
     /**
@@ -203,5 +192,16 @@ class Hoa_Test_Urg_Type_BoundFloat extends    Hoa_Test_Urg_Type_Float
     protected function getLowerBoundStatement ( ) {
 
         return $this->_lowerBoundStatement;
+    }
+
+    /**
+     * Get upper bound statement.
+     *
+     * @access  protected
+     * @return  bool
+     */
+    protected function getUpperBoundStatement ( ) {
+
+        return $this->_upperBoundStatement;
     }
 }
