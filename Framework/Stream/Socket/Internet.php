968cf6547b7c2bc70ff83b98bf7a377a448e1c5a
hywan <devnull@localhost>
2009-04-13 17:29:30 +0000

    First step for the Socket sub-package.

---
+++ Framework/Stream/Socket/Internet.php
@@ -0,0 +1,191 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Stream
+ * @subpackage  Hoa_Stream_Socket_Internet
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Stream_Socket_Exception
+ */
+import('Stream.Socket.Exception');
+
+/**
+ * Hoa_Stream_Socket_Transport
+ */
+import('Stream.Socket.Transport');
+
+/**
+ * Class Hoa_Stream_Socket_Internet.
+ *
+ * .
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Stream
+ * @subpackage  Hoa_Stream_Socket_Internet
+ */
+
+abstract class Hoa_Stream_Socket_Internet {
+
+    /**
+     * Address.
+     *
+     * @var Hoa_Stream_Socket_Internet string
+     */
+    protected $_address   = null;
+
+    /**
+     * Port.
+     *
+     * @var Hoa_Stream_Socket_Internet int
+     */
+    protected $_port      = 0;
+
+    /**
+     * Transport.
+     *
+     * @var Hoa_Stream_Socket_Internet string
+     */
+    protected $_transport = null;
+
+
+
+    /**
+     * Constructor.
+     *
+     * @access  public
+     * @param   string  $address      Address.
+     * @param   int     $port         Port.
+     * @param   string  $transport    Transport (TCP, UDP etc.).
+     * @return  void
+     */
+    public function __construct ( $address, $port, $transport = null ) {
+
+        $this->setAddress($address);
+        $this->setPort($port);
+        $this->setTransport($transport);
+
+        return;
+    }
+
+    /**
+     * Set address.
+     *
+     * @access  public
+     * @param   string  $address    Address.
+     * @return  string
+     * @throw   Hoa_Stream_Socket_Exception
+     */
+    abstract public function setAddress ( $address );
+
+    /**
+     * Set the port.
+     *
+     * @access  public
+     * @param   int     $port    Port.
+     * @return  int
+     * @throw   Hoa_Stream_Socket_Exception
+     */
+    public function setPort ( $port ) {
+
+        if($port < 0)
+            throw new Hoa_Stream_Socket_Exception(
+                'Port must be greater or equal than zero, given %d.', 0, $port);
+
+        $old         = $this->_port;
+        $this->_port = $port;
+
+        return $out;
+    }
+
+    /**
+     * Set the transport.
+     *
+     * @access  public
+     * @param   string  $transport    Transport (TCP, UDP etc.).
+     * @return  string
+     * @throw   Hoa_Stream_Socket_Exception
+     */
+    public function setTransport ( $transport ) {
+
+        $transport = strtolower($transport);
+
+        if(false === Hoa_Stream_Socket_Transport::exists($transport))
+            throw new Hoa_Stream_Socket_Exception(
+                'Transport %s is not enabled on this machin.', 1, $transport);
+
+        $old              = $this->_transport;
+        $this->_transport = strtolower($transport);
+
+        return $out;
+    }
+
+    /**
+     * Get the address.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function getAddress ( ) {
+
+        return $this->_address;
+    }
+
+    /**
+     * Get the port.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function getPort ( ) {
+
+        return $this->_port;
+    }
+
+    /**
+     * Get the transport.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function getTransport ( ) {
+
+        return $this->_transport;
+    }
+}
---
+++ Framework/Stream/Socket/Internet/Ipv4.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2008 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Stream
+ * @subpackage  Hoa_Stream_Socket_Internet_Ipv4
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_Stream_Socket_Exception
+ */
+import('Stream.Socket.Exception');
+
+/**
+ * Hoa_Stream_Socket_Internet
+ */
+import('Stream.Socket.Internet');
+
+/**
+ * Class Hoa_Stream_Socket_Internet_Ipv4.
+ *
+ * Handle IPv4.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2008 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Stream
+ * @subpackage  Hoa_Stream_Socket_Internet_Ipv4
+ */
+
+class Hoa_Stream_Socket_Internet_Ipv4 extends Hoa_Stream_Socket_Internet {
+
+    /**
+     * Set address.
+     *
+     * @access  public
+     * @param   string  $address    Address.
+     * @return  string
+     * @throw   Hoa_Stream_Socket_Exception
+     */
+    public function setAddress ( $address ) {
+
+        if(!(   substr_count($address, '.')  == 3
+             && ip2long($address)           !== false))
+            throw new Hoa_Stream_Socket_Exception(
+                'Address %s is not a valid IPv4 address.', 0, $address);
+
+        $old            = $this->_address;
+        $this->_address = $address;
+
+        return $old;
+    }
+}
