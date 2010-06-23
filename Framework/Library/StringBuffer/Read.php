c238c6c3842a2bd5a2ac53d2b77850f2749fd27c
hywan <devnull@localhost>
2010-06-23 10:04:46 +0000

    Welcome to the new package StringBuffer :-).

---
+++ Framework/Library/StringBuffer/Read.php
@@ -0,0 +1,226 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2010 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_StringBuffer
+ * @subpackage  Hoa_StringBuffer_Read
+ *
+ */
+
+/**
+ * Hoa_Framework
+ */
+require_once 'Framework.php';
+
+/**
+ * Hoa_StringBuffer_Exception
+ */
+import('StringBuffer.Exception');
+
+/**
+ * Hoa_StringBuffer
+ */
+import('StringBuffer.~');
+
+/**
+ * Hoa_Stream_Io_In
+ */
+import('Stream.Io.In');
+
+/**
+ * Class Hoa_StringBuffer_Read.
+ *
+ * Read a string buffer.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.3
+ * @package     Hoa_StringBuffer
+ * @subpackage  Hoa_StringBuffer_Read
+ */
+
+class          Hoa_StringBuffer_Read
+    extends    Hoa_StringBuffer
+    implements Hoa_Stream_Io_In {
+
+    /**
+     * Test for end-of-file.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function eof ( ) {
+
+        return feof($this->getStream());
+    }
+
+    /**
+     * Read n characters.
+     *
+     * @access  public
+     * @param   int     $length    Length.
+     * @return  string
+     * @throw   Hoa_StringBuffer_Exception
+     */
+    public function read ( $length ) {
+
+        if($length <= 0)
+            throw new Hoa_StringBuffer_Exception(
+                'Length must be greather than 0, given %d.', 0, $length);
+
+        return fread($this->getStream(), $length);
+    }
+
+    /**
+     * Alias of $this->read().
+     *
+     * @access  public
+     * @param   int     $length    Length.
+     * @return  string
+     */
+    public function readString ( $length ) {
+
+        return $this->read($length);
+    }
+
+    /**
+     * Read a character.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function readCharacter ( ) {
+
+        return fgetc($this->getStream());
+    }
+
+    /**
+     * Read a boolean.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function readBoolean ( ) {
+
+        return (bool) $this->read(1);
+    }
+
+    /**
+     * Read an integer.
+     *
+     * @access  public
+     * @param   int     $length    Length.
+     * @return  int
+     */
+    public function readInteger ( $length = 1 ) {
+
+        return (int) $this->read($length);
+    }
+
+    /**
+     * Read a float.
+     *
+     * @access  public
+     * @param   int     $length    Length.
+     * @return  float
+     */
+    public function readFloat ( $length = 1 ) {
+
+        return (float) $this->read($length);
+    }
+
+    /**
+     * Read an array.
+     * Alias of the $this->scanf() method.
+     *
+     * @access  public
+     * @param   string  $format    Format (see printf's formats).
+     * @return  array
+     */
+    public function readArray ( $format ) {
+
+        return $this->scanf($format);
+    }
+
+    /**
+     * Read a line.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function readLine ( ) {
+
+        return fgets($this->getStream());
+    }
+
+    /**
+     * Read all, i.e. read as much as possible.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function readAll ( ) {
+
+        $current = $this->tell();
+
+        $this->seek(0, Hoa_Stream_Io_Pointable::SEEK_END);
+        $end     = $this->tell();
+
+        $this->seek(0, Hoa_Stream_Io_Pointable::SEEK_SET);
+        $handle  = $this->read($end);
+
+        $this->seek($current, Hoa_Stream_Io_Pointable::SEEK_SET);
+
+        return $handle;
+    }
+
+    /**
+     * Parse input from a stream according to a format.
+     *
+     * @access  public
+     * @param   string  $format    Format (see printf's formats).
+     * @return  array
+     */
+    public function scanf ( $format ) {
+
+        return fscanf($this->getStream(), $format);
+    }
+
+    /**
+     * Transform this object to a string.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function __toString ( ) {
+
+        return $this->readAll();
+    }
+}
