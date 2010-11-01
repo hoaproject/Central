81eb861293f5f9a966910d2087e0fd77456c60f9
hywan <devnull@localhost>
2010-11-01 17:21:00 +0000

    Welcome to the new package: Hoa_Reflection :-).

---
+++ Framework/Library/Reflection/RFunction/RMethod.php
@@ -0,0 +1,408 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2010 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Reflection
+ * @subpackage  Hoa_Reflection_RFunction
+ *
+ */
+
+/**
+ * Hoa_Core
+ */
+require_once 'Core.php';
+
+/**
+ * Hoa_Reflection_RFunction_RAbstract
+ */
+import('Reflection.RFunction.RAbstract');
+
+/**
+ * Class Hoa_Reflection_RFunction.
+ *
+ * Extending ReflectionMethod capacities.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Reflection
+ * @subpackage  Hoa_Reflection_RFunction
+ */
+
+class       Hoa_Reflection_RFunction_RMethod
+    extends Hoa_Reflection_RFunction_RAbstract {
+
+    /**
+     * Method finality.
+     *
+     * @var Hoa_Reflection_RFunction_RMethod int
+     */
+    protected $_finality       = _overridable;
+
+    /**
+     * Method implementation.
+     *
+     * @var Hoa_Reflection_RFunction_RMethod int
+     */
+    protected $_implementation = _concrete;
+
+    /**
+     * Method visibility.
+     *
+     * @var Hoa_Reflection_RFunction_RMethod int
+     */
+    protected $_visibility     = _public;
+
+    /**
+     * Memory stack where the method is declared.
+     *
+     * @var Hoa_Reflection_RFunction_RMethod int
+     */
+    protected $_memoryStack    = _dynamic;
+
+
+
+    /**
+     * Reflect a method.
+     *
+     * @access  public
+     * @param   mixed    $class    Class name or ReflectionMethod instance.
+     * @param   string   $name     Method name.
+     * @return  void
+     */
+    public function __construct ( $class, $name = null ) {
+
+        if(!($class instanceof ReflectionMethod))
+            $method = new ReflectionMethod($class, $name);
+        else
+            $method = $class;
+
+        parent::__construct($method);
+
+        if(true === $method->isFinal())
+            $this->setFinality(_final);
+
+        if(true === $method->isAbstract())
+            $this->setImplementation(_abstract);
+
+        if(true === $method->isProtected())
+            $this->setVisibility(_protected);
+        elseif(true === $method->isPrivate())
+            $this->setVisibility(_private);
+
+        if(true === $method->isStatic())
+            $this->setMemoryStack(_static);
+
+        return;
+    }
+
+    /**
+     * Set the method finality.
+     *
+     * @access  public
+     * @param   int     $finality    Finality (please, see the _final and
+     *                               _overridable constants).
+     * @return  int
+     */
+    public function setFinality ( $finality ) {
+
+        $old             = $this->_finality;
+        $this->_finality = $finality;
+
+        return $old;
+    }
+
+    /**
+     * Get the method finality.
+     * Please, see the _final and _overridable constants.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function getFinality ( ) {
+
+        return $this->_finality;
+    }
+
+    /**
+     * Check if the method is final or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isFinal ( ) {
+
+        return _final == $this->getFinality();
+    }
+
+    /**
+     * Check if the method is overridable or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isOverridable ( ) {
+
+        return _overridable == $this->getFinality();
+    }
+
+    /**
+     * Set the method implementation.
+     *
+     * @access  public
+     * @param   int     $implementation    Implementation (please, see the
+     *                                     _concrete and the _abstract
+     *                                     constants).
+     * @return  int
+     */
+    public function setImplementation ( $implementation ) {
+
+        $old                   = $this->_implementation;
+        $this->_implementation = $implementation;
+
+        return $old;
+    }
+
+    /**
+     * Get the method implementation.
+     * Please, see the _concrete and the _abstract constants.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function getImplementation ( ) {
+
+        return $this->_implementation;
+    }
+
+    /**
+     * Check if the method is concrete or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isConcrete ( ) {
+
+        return _concrete == $this->getImplementation();
+    }
+
+    /**
+     * Check if the method is abstract or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isAbstract ( ) {
+
+        return _abstract == $this->getImplementation();
+    }
+
+    /**
+     * Set the method visibility.
+     *
+     * @access  public
+     * @param   int     $visibility    Visibility (please, see the _public,
+     *                                 _protected and _private constants).
+     * @return  int
+     */
+    public function setVisibility ( $visibility ) {
+
+        $old               = $this->_visibility;
+        $this->_visibility = $visibility;
+
+        return $old;
+    }
+
+    /**
+     * Get the method visibility.
+     * Please, see the _public, _protected and _private constants.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function getVisibility ( ) {
+
+        return $this->_visibility;
+    }
+
+    /**
+     * Check if the method is public or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isPublic ( ) {
+
+        return _public == $this->getVisibility();
+    }
+
+    /**
+     * Check if the method is protected or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isProtected ( ) {
+
+        return _protected == $this->getVisibility();
+    }
+
+    /**
+     * Check if the method is private or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isPrivate ( ) {
+
+        return _private == $this->getVisibility();
+    }
+
+    /**
+     * Set the method memory stack.
+     *
+     * @access  public
+     * @param   int     $stack    Memory stack (please, see the _dynamic and
+     *                            _static constants).
+     * @return  int
+     */
+    public function setMemoryStack ( $stack ) {
+
+        $old                = $this->_memoryStack;
+        $this->_memoryStack = $stack;
+
+        return $old;
+    }
+
+    /**
+     * Get the method memory stack.
+     * Please, see the _dynamic and _static constants.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function getMemoryStack ( ) {
+
+        return $this->_memoryStack;
+    }
+
+    /**
+     * Check if the method is dynamic or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isDynamic ( ) {
+
+        return _dynamic == $this->getMemoryStack();
+    }
+
+    /**
+     * Check if the method is static or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isStatic ( ) {
+
+        return _static == $this->getMemoryStack();
+    }
+
+    /**
+     * Check if the method is a constructor or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isConstructor ( ) {
+
+        return '__construct' == $this->getName();
+    }
+
+    /**
+     * Check if the method is a destructor or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isDestructor ( ) {
+
+        return '__destruct' == $this->getName();
+    }
+
+    /**
+     * Pretty-printer.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function __toString ( ) {
+
+        $tab = '    ';
+        $out = $tab .
+               str_replace("\n", "\n" . $tab, $this->getComment()) .
+               "\n" . $tab;
+
+        if(true === $this->isAbstract())
+            $out .= 'abstract ';
+
+        if(true === $this->isPublic())
+            $out .= 'public ';
+        elseif(true === $this->isProtected())
+            $out .= 'protected ';
+        elseif(true === $this->isPrivate())
+            $out .= 'private ';
+
+        if(true === $this->isStatic())
+            $out .= 'static ';
+
+        if(true === $this->getReference())
+            $out .= '&';
+
+        $out   .= 'function ' . $this->getName() . ' (';
+        $first  = true;
+        $handle = null;
+
+        foreach($this->getParameters() as $i => $parameter) {
+
+            if(false === $first)
+                $handle .= ',';
+            else
+                $first = false;
+
+            $handle .= ' ' . $parameter;
+        }
+
+        $out .= $handle . ' ) {' . "\n\n" .
+                $this->getBody() . "\n" . $tab . '}';
+
+        return $out;
+    }
+}
---
+++ Framework/Library/Reflection/RParameter.php
@@ -0,0 +1,386 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2010 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Reflection
+ * @subpackage  Hoa_Reflection_RParameter
+ *
+ */
+
+/**
+ * Hoa_Core
+ */
+require_once 'Core.php';
+
+/**
+ * Hoa_Reflection_Wrapper
+ */
+import('Reflection.Wrapper');
+
+/**
+ * Hoa_Reflection_RClass
+ */
+import('Reflection.RClass');
+
+/**
+ * Class Hoa_Reflection_RParameter.
+ *
+ * Extending ReflectionParameter capacities.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Reflection
+ * @subpackage  Hoa_Reflection_RParameter
+ */
+
+class Hoa_Reflection_RParameter extends Hoa_Reflection_Wrapper {
+
+    /**
+     * Parameter type (a string or an object).
+     *
+     * @var Hoa_Reflection_RParameter mixed
+     */
+    protected $_type         = null;
+
+    /**
+     * Whether the parameter is passed by reference.
+     *
+     * @var Hoa_Reflection_RParameter bool
+     */
+    protected $_byReference  = false;
+
+    /**
+     * Parameter name.
+     *
+     * @var Hoa_Reflection_RParameter string
+     */
+    protected $_name         = null;
+
+    /**
+     * Parameter default value.
+     *
+     * @var Hoa_Reflection_RParameter mixed
+     */
+    protected $_defaultValue = null;
+
+    /**
+     * Whether the parameter is optional or not.
+     *
+     * @var Hoa_Reflection_RParameter bool
+     */
+    protected $_optional     = false;
+
+    /**
+     * Parameter position.
+     *
+     * @var Hoa_Reflection_RParameter int
+     */
+    protected $_position     = 0;
+
+
+
+    /**
+     * Reflect a parameter.
+     *
+     * @access  public
+     * @param   mixed   $function     Function owning parameter or
+     *                                ReflectionParameter instance.
+     * @param   string  $parameter    Parameter's name.
+     * @return  void
+     */
+    public function __construct ( $function, $parameter = null ) {
+
+        if($function instanceof ReflectionParameter)
+            $p = $function;
+        else
+            $p = new ReflectionParameter($function, $parameter);
+
+        $this->setWrapped($p);
+        $this->setType($p->isArray() ? 'Array' : $p->getClass());
+        $this->setReference($p->isPassedByReference());
+        $this->setName($p->getName());
+
+        if(true === $p->isDefaultValueAvailable()) {
+
+            $this->setDefaultValue($p->getDefaultValue());
+            $this->setOptional(true);
+        }
+
+        $this->_position = $p->getPosition();
+
+        return;
+    }
+
+    /**
+     * Set parameter type.
+     *
+     * @access  public
+     * @param   mixed   $type    Type (a string or an object).
+     * @return  mixed
+     */
+    public function setType ( $type ) {
+
+        if($type instanceof ReflectionClass)
+            $type = new Hoa_Reflection_RClass($type);
+
+        $old         = $this->_type;
+        $this->_type = $type;
+
+        return $old;
+    }
+
+    /**
+     * Get parameter type.
+     *
+     * @access  public
+     * @return  mixed
+     */
+    public function getType ( ) {
+
+        return $this->_type;
+    }
+
+    /**
+     * Get parameter type as a string.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function getTypeAsString ( ) {
+
+        $type = $this->getType();
+
+        if(!is_object($type))
+            return $type;
+
+        if(   ($type instanceof ReflectionClass)
+           || ($type instanceof Hoa_Reflection_RClass))
+            return $type->getName();
+
+        return get_class($type);
+    }
+
+    /**
+     * Check if the parameter has a type.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function hasType ( ) {
+
+        return null !== $this->_type;
+    }
+
+    /**
+     * Set whether the parameter is passed by reference.
+     *
+     * @access  public
+     * @param   bool    $reference    Whether parameter is passed by reference
+     *                                or not.
+     * @return  bool
+     */
+    public function setReference ( $reference ) {
+
+        $old                = $this->_byReference;
+        $this->_byReference = $reference;
+
+        return $old;
+    }
+
+    /**
+     * Get whether the parameter is passed by reference.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function getReference ( ) {
+
+        return $this->_byReference;
+    }
+
+    /**
+     * Override the ReflectionParameter method.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isPassedByReference ( ) {
+
+        return $this->getReference();
+    }
+
+    /**
+     * Set the parameter name.
+     *
+     * @access  public
+     * @param   string  $name    Name.
+     * @return  string
+     */
+    public function setName ( $name ) {
+
+        $old         = $this->_name;
+        $this->_name = $name;
+
+        return $old;
+    }
+
+    /**
+     * Get the parameter name.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function getName ( ) {
+
+        return $this->_name;
+    }
+
+    /**
+     * Set the default value. Do not forget to call self::setOptional() if
+     * necessary.
+     *
+     * @access  public
+     * @param   mixed   $value    Default value.
+     * @return  mixed
+     */
+    public function setDefaultValue ( $value ) {
+
+        $old                 = $this->_defaultValue;
+        $this->_defaultValue = $value;
+
+        return $old;
+    }
+
+    /**
+     * Get the default value.
+     *
+     * @access  public
+     * @return  mixed
+     */
+    public function getDefaultValue ( ) {
+
+        return $this->_defaultValue;
+    }
+
+    /**
+     * Set whether the parameter is optinal or not.
+     *
+     * @access  public
+     * @param   bool    $optional    Whether the parameter is optional.
+     * @return  bool
+     */
+    public function setOptional ( $optional ) {
+
+        $old             = $this->_optional;
+        $this->_optional = $optional;
+
+        return $old;
+    }
+
+    /**
+     * Get whether the parameter is optional or not.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function getOptional ( ) {
+
+        return $this->_optional;
+    }
+
+    /**
+     * Override the ReflectionParameter method.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isOptional ( ) {
+
+        return $this->getOptional();
+    }
+
+    /**
+     * Override the ReflectionParameter method.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function isDefaultValueAvailable ( ) {
+
+        return $this->getOptional();
+    }
+
+    /**
+     * Override the ReflectionParameter method.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function allowsNull ( ) {
+
+        return true;
+    }
+
+    /**
+     * Get the parameter position.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function getPosition ( ) {
+
+        return $this->_position;
+    }
+
+    /**
+     * Pretty-printer.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function __toString ( ) {
+
+        $out = null;
+
+        if(true === $this->hasType())
+            $out .= $this->getTypeAsString() . ' ';
+
+        if(true === $this->getReference())
+            $out .= '&';
+
+        $out .= '$' . $this->getName();
+
+        if(true === $this->isOptional())
+            $out .= ' = ' . var_export($this->getDefaultValue(), true);
+
+        return $out;
+    }
+}
