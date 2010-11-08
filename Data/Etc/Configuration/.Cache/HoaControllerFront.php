bad89aa24311f35ad4ad7ee589476a57cf9793a9
hywan <devnull@localhost>
2010-11-08 08:30:44 +0000

    Update cache.

--- Data/Etc/Configuration/.Cache/HoaControllerFront.php
+++ Data/Etc/Configuration/.Cache/HoaControllerFront.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Generated the 2010-05-16T11:06:07.000000Z
+ * Generated the 2010-11-08T09:29:37.000000Z
  */
 
 return array (
--- Data/Etc/Configuration/.Cache/HoaTest.php
+++ Data/Etc/Configuration/.Cache/HoaTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Generated the 2010-05-16T11:06:07.000000Z
+ * Generated the 2010-11-08T09:29:37.000000Z
  */
 
 return array (
@@ -10,18 +10,13 @@ return array (
   ),
   'parameters' => 
   array (
-    'convict.directory' => NULL,
-    'convict.recursive' => true,
-    'convict.result' => 
-    array (
-    ),
+    'convict' => NULL,
     'root' => 'hoa://Data/Variable/Test/',
     'repository' => '(:%root:)Repository/',
     'revision' => '(:_YmdHis:)/',
     'incubator' => '(:%repository:)(:%revision:)Incubator/',
-    'ordeal.oracle' => '(:%repository:)(:%revision:)Ordeal/Oracle/',
-    'ordeal.battleground' => '(:%repository:)(:%revision:)Ordeal/Battleground/',
-    'oracle.eyes.methodPrefix' => '__hoa_',
+    'instrumented' => '(:%repository:)(:%revision:)Instrumented/',
+    'sampler' => '(:%repository:)(:%revision:)Sampler/',
     'dictionary' => '(:%root:)Dictionary/',
     'maxtry' => 64,
   ),
