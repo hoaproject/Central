ec81372ac453307ba1af9b0b0209d535fe72a402
hywan <devnull@localhost>
2010-11-21 01:50:39 +0000

    Remove the require_once 'Core.php' instruction to remove all include_path dependencies.

--- Framework/Library/Session/Exception/SessionIsExpired.php
+++ Framework/Library/Session/Exception/SessionIsExpired.php
@@ -33,11 +33,6 @@
  */
 
 /**
- * Hoa_Core
- */
-require_once 'Core.php';
-
-/**
  * Hoa_Session_Exception
  */
 import('Session.Exception');
--- Framework/Library/Session/Session.php
+++ Framework/Library/Session/Session.php
@@ -32,11 +32,6 @@
  */
 
 /**
- * Hoa_Core
- */
-require_once 'Core.php';
-
-/**
  * Hoa_Session_Exception
  */
 import('Session.Exception');
