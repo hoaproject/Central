1047b384e7c181dec2fb919e76e5a40f5495c962
hywan <devnull@localhost>
2010-11-21 01:50:39 +0000

    Remove the require_once 'Core.php' instruction to remove all include_path dependencies.

--- Framework/Library/Memory/GarbageCollector.php
+++ Framework/Library/Memory/GarbageCollector.php
@@ -33,11 +33,6 @@
  */
 
 /**
- * Hoa_Core
- */
-require_once 'Core.php';
-
-/**
  * Hoa_Memory_Exception
  */
 import('Memory.Exception');
--- Framework/Library/Memory/Memory.php
+++ Framework/Library/Memory/Memory.php
@@ -32,11 +32,6 @@
  */
 
 /**
- * Hoa_Core
- */
-require_once 'Core.php';
-
-/**
  * Hoa_Memory_Exception
  */
 import('Memory.Exception');
