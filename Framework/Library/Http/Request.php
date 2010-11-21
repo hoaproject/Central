1947b11199cde2cbf82be9ff1ab1b8cde2725bd6
hywan <devnull@localhost>
2010-11-21 01:18:52 +0000

    Write setters/getters.

--- Framework/Library/Http/Request.php
+++ Framework/Library/Http/Request.php
@@ -73,6 +73,27 @@ class Hoa_Http_Request {
     protected $_url         = null;
     protected $_httpVersion = null;
 
+    protected $_accept             = null;
+    protected $_acceptCharset      = null;
+    protected $_acceptEncoding     = null;
+    protected $_acceptLanguage     = null;
+    protected $_authorization      = null;
+    protected $_connection         = null;
+    protected $_except             = null;
+    protected $_from               = null;
+    protected $_ifMatch            = null;
+    protected $_ifModifiedSince    = null;
+    protected $_ifNoneMatch        = null;
+    protected $_ifRange            = null;
+    protected $_ifUnmodifiedSince  = null;
+    protected $_keepAlive          = null;
+    protected $_maxForwards        = null;
+    protected $_proxyAuthorization = null;
+    protected $_range              = null;
+    protected $_referer            = null;
+    protected $_te                 = null;
+    protected $_userAgent          = null;
+
     public function __construct ( $headers = null ) {
 
         if(null !== $headers)
@@ -90,7 +111,7 @@ class Hoa_Http_Request {
 
         if(0 === preg_match('#^(\w+)\s*([^\s*]+)\s*HTTP/(1\..?)$#', $http, $matches))
             throw new Hoa_Http_Exception(
-                'HTTP headers are not well-formed.', 0);
+                'HTTP headers are not well-formed: %', 0, $http);
 
         switch(strtolower($matches[1])) {
 
@@ -130,8 +151,8 @@ class Hoa_Http_Request {
                 $this->_method = self::METHOD_EXTENDED;
         }
 
-        $this->_url         = $matches[2];
-        $this->_httpVersion = (float) $matches[3];
+        $this->_url         = $this->setURL($matches[2]);
+        $this->_httpVersion = $this->setHTTPVersion((float) $matches[3]);
 
         foreach($headers as $header) {
 
@@ -140,10 +161,6 @@ class Hoa_Http_Request {
             $fieldValue = trim(substr($header, $semicolon + 1));
 
             switch($fieldName) {
-
-                case 'host':
-                    $this->setHost($fieldValue);
-                  break;
 
                 case 'accept':
                     $this->setAccept($fieldValue);
@@ -163,6 +180,10 @@ class Hoa_Http_Request {
 
                 case 'authorization':
                     $this->setAuthorization($fieldValue);
+                  break;
+
+                case 'connection':
+                    $this->setConnection($fieldValue);
                   break;
 
                 case 'except':
@@ -197,6 +218,10 @@ class Hoa_Http_Request {
                     $this->setIfUnmodifiedSince($fieldValue);
                   break;
 
+                case 'keep-alive':
+                    $this->setKeepAlive($fieldValue);
+                  break;
+
                 case 'max-forwards':
                     $this->setMaxForwards($fieldValue);
                   break;
@@ -219,10 +244,11 @@ class Hoa_Http_Request {
 
                 case 'user-agent':
                     $this->setUserAgent($fieldValue);
+                  break;
 
                 default:
-                    var_dump($fieldName, $fieldValue);
-                    echo "\n";
+                    //var_dump($fieldName, $fieldValue);
+                    //echo "\n";
             }
         }
 
@@ -237,8 +263,307 @@ class Hoa_Http_Request {
         return $old;
     }
 
+    public function getMethod ( ) {
+
+        return $this->_method;
+    }
+
+    public function setAccept ( $accept ) {
+
+        $old           = $this->_accept;
+        $this->_accept = $accept;
+
+        return $old;
+    }
+
+    public function getAccept ( ) {
+
+        return $this->_accept;
+    }
+
+    public function setAcceptCharset ( $acceptCharset ) {
+
+        $old                  = $this->_acceptCharset;
+        $this->_acceptCharset = $acceptCharset;
+
+        return $old;
+    }
+
+    public function getAcceptCharset ( ) {
+
+        return $this->_acceptCharset;
+    }
+
+    public function setAcceptEncoding ( $acceptEncoding ) {
+
+        $old                   = $this->_acceptEncoding;
+        $this->_acceptEncoding = $acceptEncoding;
+
+        return $old;
+    }
+
+    public function getAcceptEncoding ( ) {
+
+        return $this->_acceptEncoding;
+    }
+
+    public function setAcceptLanguage ( $acceptLanguage ) {
+
+        $old                   = $this->_acceptLanguage;
+        $this->_acceptLanguage = $acceptLanguage;
+
+        return $old;
+    }
+
+    public function getAcceptLanguage ( ) {
+
+        return $this->_acceptLanguage;
+    }
+
+    public function setAuthorization ( $authorization ) {
+
+        $old                  = $this->_authorization;
+        $this->_authorization = $authorization;
+
+        return $old;
+    }
+
+    public function getAuthorization ( ) {
+
+        return $this->_authorization;
+    }
+
+    public function setConnection ( $connection ) {
+
+        $old               = $this->_connection;
+        $this->_connection = $connection;
+
+        return $old;
+    }
+
+    public function getConnection ( ) {
+
+        return $this->_connection;
+    }
+
+    public function setExcept ( $except ) {
+
+        $old           = $this->_except;
+        $this->_except = $except;
+
+        return $old;
+    }
+
+    public function getExcept ( ) {
+
+        return $this->_except;
+    }
+
+    public function setFrom ( $from ) {
+
+        $old         = $this->_from;
+        $this->_from = $from;
+
+        return $old;
+    }
+
+    public function getFrom ( ) {
+
+        return $this->_from;
+    }
+
     public function setHost ( $host ) {
 
-        
+        $old         = $this->_host;
+        $this->_host = $host;
+
+        return $old;
+    }
+
+    public function getHost ( ) {
+
+        return $this->_host;
+    }
+
+    public function setHTTPVersion ( $version ) {
+
+        $old                = $this->_httpVersion;
+        $this->_httpVersion = $version;
+
+        return $old;
+    }
+
+    public function getHTTPVersion ( ) {
+
+        return $this->_httpVersion;
+    }
+
+    public function setIfMatch ( $ifMatch ) {
+
+        $old            = $this->_ifMatch;
+        $this->_ifMatch = $ifMatch;
+
+        return $old;
+    }
+
+    public function getIfMatch ( ) {
+
+        return $this->_ifMatch;
+    }
+
+    public function setIfModifiedSince ( $ifModifiedSince ) {
+
+        $old                    = $this->_ifModifiedSince;
+        $this->_ifModifiedSince = $ifModifiedSince;
+
+        return $old;
+    }
+
+    public function getIfModifiedSince ( ) {
+
+        return $this->_ifModifiedSince;
+    }
+
+    public function setIfNoneMatch ( $ifNoneMatch ) {
+
+        $old                = $this->_ifNoneMatch;
+        $this->_ifNoneMatch = $ifNoneMatch;
+
+        return $old;
+    }
+
+    public function getIfNoneMatch ( ) {
+
+        return $this->_ifNoneMatch;
+    }
+
+    public function setIfRange ( $ifRange ) {
+
+        $old            = $this->_ifRange;
+        $this->_ifRange = $ifRange;
+
+        return $old;
+    }
+
+    public function getIfRange ( ) {
+
+        return $this->_ifRange;
+    }
+
+    public function setIfUnmodifiedSince ( $ifUnmodifiedSince ) {
+
+        $old                      = $this->_ifUnmodifiedSince;
+        $this->_ifUnmodifiedSince = $ifUnmodifiedSince;
+
+        return $old;
+    }
+
+    public function getIfUnmodifiedSince ( ) {
+
+        return $this->_ifUnmodifiedSince;
+    }
+
+    public function setKeepAlive ( $keepAlive ) {
+
+        $old              = $this->_keepAlive;
+        $this->_keepAlive = $keepAlive;
+
+        return $old;
+    }
+
+    public function getKeepAlive ( ) {
+
+        return $this->_keepAlive;
+    }
+
+    public function setMaxForwards ( $maxForwards ) {
+
+        $old                = $this->_maxForwards;
+        $this->_maxForwards = $maxForwards;
+
+        return $old;
+    }
+
+    public function getMaxForwards ( ) {
+
+        return $this->_maxForwards;
+    }
+
+    public function setProxyAuthorization ( $proxyAuthorization ) {
+
+        $old                       = $this->_proxyAuthorization;
+        $this->_proxyAuthorization = $proxyAuthorization;
+
+        return $old;
+    }
+
+    public function getProxyAuthrization ( ) {
+
+        return $this->_proxyAuthorization;
+    }
+
+    public function setRange ( $range ) {
+
+        $old          = $this->_range;
+        $this->_range = $range;
+
+        return $old;
+    }
+
+    public function getRange ( ) {
+
+        return $this->_range;
+    }
+
+    public function setReferer ( $referer ) {
+
+        $old            = $this->_referer;
+        $this->_referer = $referer;
+
+        return $old;
+    }
+
+    public function getReferer ( ) {
+
+        return $this->_referer;
+    }
+
+    public function setTE ( $te ) {
+
+        $old       = $this->_te;
+        $this->_te = $te;
+
+        return $old;
+    }
+
+    public function getTE ( ) {
+
+        return $this->_te;
+    }
+
+    public function setURL ( $url ) {
+
+        $old        = $this->_url;
+        $this->_url = $url;
+
+        return;
+    }
+
+    public function getURL ( ) {
+
+        return $this->_url;
+    }
+
+    public function setUserAgent ( $userAgent ) {
+
+        $old              = $this->_userAgent;
+        $this->_userAgent = $userAgent;
+
+        return $old;
+    }
+
+    public function getUserAgent ( ) {
+
+        return $this->_userAgent;
     }
 }
