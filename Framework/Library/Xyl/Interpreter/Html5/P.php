d87fa5ae709e139fb46fa87e757b934c42ba7e67
hywan <devnull@localhost>
2010-12-06 11:17:00 +0000

    Start to write XYL components.

--- Framework/Library/Xyl/Interpreter/Html5/P.php
+++ Framework/Library/Xyl/Interpreter/Html5/P.php
@@ -1,25 +1,7 @@
 <?php
 
-class Hoa_Xyl_Interpreter_Html5_P extends Hoa_Xyl_Element_Concrete {
+class       Hoa_Xyl_Interpreter_Html5_P
+    extends Hoa_Xyl_Interpreter_Html5_Generic {
 
-    public function paint ( Hoa_Stream_Interface_Out $out ) {
-
-        $e = $this->getAbstractElement();
-
-        $out->writeAll('<p' .
-                       $e->readAttributesAsString() .
-                       '>');
-
-        $out->writeAll($e . '');
-
-        foreach($this as $name => $child) {
-
-            $out->writeAll("\n");
-            $child->render($out);
-        }
-
-        $out->writeAll('</p>' . "\n");
-
-        return;
-    }
+    protected $_map = 'p';
 }
--- Framework/Library/Xyl/Interpreter/Html5/Section1.php
+++ Framework/Library/Xyl/Interpreter/Html5/Section1.php
@@ -1,20 +1,7 @@
 <?php
 
-class Hoa_Xyl_Interpreter_Html5_Section1 extends Hoa_Xyl_Element_Concrete {
+class       Hoa_Xyl_Interpreter_Html5_Section1
+    extends Hoa_Xyl_Interpreter_Html5_Generic {
 
-    public function paint ( Hoa_Stream_Interface_Out $out ) {
-
-        $e = $this->getAbstractElement();
-
-        if($e->attributeExists('bind')) {
-
-            $out->writeAll('<h1>' . $this->getCurrentData() . '</h1>'.  "\n");
-
-            return;
-        }
-
-        $out->writeAll('<h1>' . $e . '</h1>' . "\n");
-
-        return;
-    }
+    protected $_map = 'h1';
 }
