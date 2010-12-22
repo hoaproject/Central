5e47c3b7461d4aa245f86357e2b1c93fcd3fad2f
hywan <devnull@localhost>
2010-12-22 17:40:46 +0000

    Add Hoa_Xyl configuration.

---
+++ Data/Etc/Configuration/.Cache/HoaXyl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Generated the 2010-12-22T18:39:30.000000Z
+ */
+
+return array (
+  'keywords' => 
+  array (
+    'theme' => 'Classic',
+  ),
+  'parameters' => 
+  array (
+    'theme' => '(:theme:lU:)',
+    'html5.css' => 'hoa://Application/Public/(:%theme:)/Css/',
+    'html5.font' => 'hoa://Application/Public/(:%theme:)/Font/',
+    'html5.image' => 'hoa://Application/Public/(:%theme:)/Image/',
+    'html5.javascript' => 'hoa://Application/Public/(:%theme:)/Javascript/',
+    'html5.video' => 'hoa://Application/Public/(:%theme:)/Video/',
+  ),
+);
