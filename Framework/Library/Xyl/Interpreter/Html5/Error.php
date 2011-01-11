8ceeaefbbf2f9bdb74c6300fdd1b07a48e052d9b
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-01-11 09:33:26 +0100

    Add <break /> and <error /> components.

---
+++ Framework/Library/Xyl/Interpreter/Html5/Error.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2010 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @category    Framework
+ * @package     Hoa_Xyl
+ * @subpackage  Hoa_Xyl_Interpreter_Html5_Error
+ *
+ */
+
+/**
+ * Hoa_Xyl_Element_Concrete
+ */
+import('Xyl.Element.Concrete') and load();
+
+/**
+ * Hoa_Xyl_Element_Executable
+ */
+import('Xyl.Element.Executable') and load();
+
+/**
+ * Hoa_Xml_Element_Model_Phrasing
+ */
+import('Xml.Element.Model.Phrasing') and load();
+
+/**
+ * Class Hoa_Xyl_Interpreter_Html5_Error.
+ *
+ * The <error /> component.
+ *
+ * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
+ * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @since       PHP 5
+ * @version     0.1
+ * @package     Hoa_Xyl
+ * @subpackage  Hoa_Xyl_Interpreter_Html5_Error
+ */
+
+class          Hoa_Xyl_Interpreter_Html5_Error
+    extends    Hoa_Xyl_Element_Concrete
+    implements Hoa_Xml_Element_Model_Phrasing {
+
+    /**
+     * Visibility.
+     *
+     * @var Hoa_Xyl_Element_Concrete bool
+     */
+    protected $_visibility = false;
+
+
+
+    /**
+     * Paint the element.
+     *
+     * @access  protected
+     * @param   Hoa_Stream_Interface_Out  $out    Out stream.
+     * @return  void
+     */
+    protected function paint ( Hoa_Stream_Interface_Out $out ) {
+
+        $out->writeAll('<div class="error"' .
+                       $this->readAttributesAsString() . '>');
+
+        foreach($this as $child)
+            $child->render($out);
+
+        $out->writeAll('</div>' . "\n");
+
+        return;
+    }
+}
