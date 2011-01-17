4ca53696ac209874b227745bb68a2c5a6cad4608
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-01-17 16:36:55 +0100

    Mania: simplify code.

--- Framework/Library/Xyl/Interpreter/Common/Yield.php
+++ Framework/Library/Xyl/Interpreter/Common/Yield.php
@@ -62,7 +62,7 @@ class Hoa_Xyl_Interpreter_Common_Yield extends Hoa_Xyl_Element_Concrete {
      */
     public function paint ( Hoa_Stream_Interface_Out $out ) {
 
-        foreach($this as $name => $child)
+        foreach($this as $child)
             $child->render($out);
 
         return;
