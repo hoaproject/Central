ff86e48a4af2519bb808a970bddfc6439d430dbc
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-07 11:59:33 +0100

    Change Hoa_Exception for Hoa_Core_Exception.

--- Framework/Library/Database/Database.php
+++ Framework/Library/Database/Database.php
@@ -198,7 +198,7 @@ class Hoa_Database implements Hoa_Core_Parameterizable {
      * @access  public
      * @param   array   $in    Parameters to set.
      * @return  void
-     * @throw   Hoa_Exception
+     * @throw   Hoa_Core_Exception
      */
     public function setParameters ( Array $in ) {
 
@@ -210,7 +210,7 @@ class Hoa_Database implements Hoa_Core_Parameterizable {
      *
      * @access  public
      * @return  array
-     * @throw   Hoa_Exception
+     * @throw   Hoa_Core_Exception
      */
     public function getParameters ( ) {
 
@@ -224,7 +224,7 @@ class Hoa_Database implements Hoa_Core_Parameterizable {
      * @param   string  $key      Key.
      * @param   mixed   $value    Value.
      * @return  mixed
-     * @throw   Hoa_Exception
+     * @throw   Hoa_Core_Exception
      */
     public function setParameter ( $key, $value ) {
 
@@ -237,7 +237,7 @@ class Hoa_Database implements Hoa_Core_Parameterizable {
      * @access  public
      * @param   string  $key    Key.
      * @return  mixed
-     * @throw   Hoa_Exception
+     * @throw   Hoa_Core_Exception
      */
     public function getParameter ( $key ) {
 
@@ -251,7 +251,7 @@ class Hoa_Database implements Hoa_Core_Parameterizable {
      * @access  public
      * @param   string  $key    Key.
      * @return  mixed
-     * @throw   Hoa_Exception
+     * @throw   Hoa_Core_Exception
      */
     public function getFormattedParameter ( $key ) {
 
