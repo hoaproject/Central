019868de14ff6b1ae5afe18b7b6acb6ff52dda26
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-15 17:42:36 +0100

    Add the <image /> component.

---
+++ Framework/Library/Xyl/Interpreter/Html/Image.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2011 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Xyl\Element\Concrete
+ */
+-> import('Xyl.Element.Concrete')
+
+/**
+ * \Hoa\Xyl\Element\Executable
+ */
+-> import('Xyl.Element.Executable');
+
+}
+
+namespace Hoa\Xyl\Interpreter\Html {
+
+/**
+ * Class \Hoa\Xyl\Interpreter\Html\Image.
+ *
+ * The <image /> component.
+ *
+ * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @license    http://gnu.org/licenses/gpl.txt GNU GPL
+ */
+
+class          Image
+    extends    \Hoa\Xyl\Element\Concrete
+    implements \Hoa\Xyl\Element\Executable {
+
+    /**
+     * Paint the element.
+     *
+     * @access  protected
+     * @param   \Hoa\Stream\IStream\Out  $out    Out stream.
+     * @return  void
+     */
+    protected function paint ( \Hoa\Stream\IStream\Out $out ) {
+
+        $out->writeAll('<img' . $this->readAttributesAsString() . ' />');
+
+        return;
+    }
+
+    /**
+     * Execute an element.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function execute ( ) {
+
+        $this->writeAttribute('src', resolve($this->readAttribute('href')));
+        $this->removeAttribute('href');
+
+        return;
+    }
+}
+
+}
