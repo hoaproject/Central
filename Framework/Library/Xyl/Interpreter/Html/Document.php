7a9d71a599ded7dea1d9a639ebe834f1aada6b23
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-15 13:15:48 +0100

    Change Html5 for Html.

---
+++ Framework/Library/Xyl/Interpreter/Html/Document.php
@@ -0,0 +1,246 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2011 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Xyl\Element\Concrete
+ */
+-> import('Xyl.Element.Concrete')
+
+/**
+ * \Hoa\Xyl\Element\Executable
+ */
+-> import('Xyl.Element.Executable');
+
+}
+
+namespace Hoa\Xyl\Interpreter\Html {
+
+/**
+ * Class \Hoa\Xyl\Interpreter\Html\Document.
+ *
+ * The <document /> component.
+ *
+ * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @license    http://gnu.org/licenses/gpl.txt GNU GPL
+ */
+
+class          Document
+    extends    \Hoa\Xyl\Element\Concrete
+    implements \Hoa\Xyl\Element\Executable {
+
+    /**
+     * Title.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html\Title object
+     */
+    protected $_title     = null;
+
+    /**
+     * All document resources.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html\Document array
+     */
+    protected $_resources = array();
+
+
+
+    /**
+     * Paint the element.
+     *
+     * @access  protected
+     * @param   \Hoa\Stream\IStream\Out  $out    Out stream.
+     * @return  void
+     */
+    protected function paint ( \Hoa\Stream\IStream\Out $out ) {
+
+        $root = $this->getAbstractElementSuperRoot();
+
+        $out->writeAll(
+            '<!DOCTYPE html>' . "\n\n" .
+            '<!--[if lt IE 7]><html class="ie6"><![endif]-->' . "\n" .
+            '<!--[if    IE 7]><html class="ie7"><![endif]-->' . "\n" .
+            '<!--[if    IE 8]><html class="ie8"><![endif]-->' . "\n" .
+            '<!--[if (gte IE 9)|!(IE)]>' . "\n" .
+            '<html>' . "\n" .
+            '<![endif]-->' . "\n" .
+            '<head>' . "\n" .
+            '  <title>'
+        );
+
+        if(null !== $this->_title)
+            $this->_title->render($out);
+
+        $out->writeAll('</title>' . "\n");
+
+        if(isset($this->_resources['css']))
+            foreach($this->_resources['css'] as $href)
+                $out->writeAll(
+                    '  <link type="text/css" href="' .
+                    $root->resolve($href) .
+                    '" rel="stylesheet" />' . "\n"
+                );
+
+        $out->writeAll(
+            '</head>' . "\n" .
+            '<body>' . "\n\n" .
+            '<div id="body">' . "\n"
+        );
+
+        foreach($this as $child)
+            if('title' != $child->getName())
+                $child->render($out);
+
+        $out->writeAll(
+            "\n" . '</div>' . "\n\n" . '</body>' . "\n" . '</html>'
+        );
+
+        return;
+    }
+
+    /**
+     * Execute an element.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function execute ( ) {
+
+        $this->computeTitle();
+        $this->computeStylesheet();
+
+        return;
+    }
+
+    /**
+     * Compute title.
+     *
+     * @access  protected
+     * @return  void
+     */
+    protected function computeTitle ( ) {
+
+        $xpath = $this->xpath('./__current_ns:*[1]');
+
+        if(empty($xpath))
+            return;
+
+        $title = $this->getConcreteElement($xpath[0]);
+
+        if(!($title instanceof Title))
+            return;
+
+        $this->_title = $title;
+
+        return;
+    }
+
+    /**
+     * Compute stylesheet.
+     *
+     * @access  protected
+     * @return  void
+     */
+    protected function computeStylesheet ( ) {
+
+        $root                    = $this->getAbstractElementSuperRoot();
+        $router                  = $root->getRouter();
+        $styles                  = $root->getStylesheets();
+        $theme                   = $root->getTheme();
+        $this->_resources['css'] = array();
+
+        foreach($styles as $style)
+            if('hoa://Library/Xyl/Css/' == substr($style, 0, 22)) {
+
+                $resolved = $root->resolve($style);
+
+                if(false === file_exists($resolved))
+                    continue;
+
+                $redirect = $root->resolve(
+                    'hoa://Application/Public/' . substr($style, 18)
+                );
+
+                if(false === file_exists($redirect))
+                    if(false === copy($resolved, $redirect))
+                        throw new Exception(
+                            'Failed to copy %s in %s.',
+                            0, array($style, $redirect));
+
+                if(null === $router)
+                    $this->_resources['css'][] = $resolved;
+                else
+                    $this->_resources['css'][] = $router->unroute(
+                        '_css',
+                        array(
+                            'theme' => $theme,
+                            'sheet' => substr($style, 22)
+                        )
+                    );
+            }
+            elseif('hoa://Application/Public/Css/' == substr($style, 0, 29)) {
+
+                $resolved = $root->resolve($style);
+
+                if(false === file_exists($resolved))
+                    continue;
+
+                if(null === $router)
+                    $this->_resources['css'][] = $resolved;
+                else
+                    $this->_resources['css'][] = $router->unroute(
+                        '_css',
+                        array(
+                            'theme' => $theme,
+                            'sheet' => substr($style, 29)
+                        )
+                    );
+            }
+            else
+                $this->_resources['css'][] = $style;
+
+        return;
+    }
+
+    /**
+     * Get the <title /> component.
+     *
+     * @access  public
+     * @return  \Hoa\Xyl\Interpreter\Html\Title
+     */
+    public function getTitle ( ) {
+
+        return $this->_title;
+    }
+}
+
+}
---
+++ Framework/Library/Xyl/Interpreter/Html/Html.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * Hoa Framework
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of HOA Open Accessibility.
+ * Copyright (c) 2007, 2011 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Xyl\Interpreter
+ */
+-> import('Xyl.Interpreter.~')
+
+/**
+ * \Hoa\Xyl\Interpreter\Common\*
+ */
+-> import('Xyl.Interpreter.Common.*')
+
+/**
+ * \Hoa\Xyl\Interpreter\Html\*
+ */
+-> import('Xyl.Interpreter.Html.*');
+
+}
+
+namespace Hoa\Xyl\Interpreter\Html {
+
+/**
+ * Class \Hoa\Xyl\Interpreter\Html.
+ *
+ * HTML5 intepreter.
+ *
+ * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @license    http://gnu.org/licenses/gpl.txt GNU GPL
+ */
+
+class Html extends \Hoa\Xyl\Interpreter { 
+
+    /**
+     * Rank: abstract elements to concrete elements.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html array
+     */
+    protected $_rank         = array(
+        // XYL.
+        'document'        => '\Hoa\Xyl\Interpreter\Html\Document',
+        'yield'           => '\Hoa\Xyl\Interpreter\Common\Yield',
+
+        // Layout.
+        'box'             => '\Hoa\Xyl\Interpreter\Html\Box',
+        'hbox'            => '\Hoa\Xyl\Interpreter\Html\Hbox',
+        'vbox'            => '\Hoa\Xyl\Interpreter\Html\Vbox',
+        'grid'            => '\Hoa\Xyl\Interpreter\Html\Grid',
+        'grows'           => '\Hoa\Xyl\Interpreter\Html\Grows',
+        'grow'            => '\Hoa\Xyl\Interpreter\Html\Grow',
+
+        // Section.
+        'section1'        => '\Hoa\Xyl\Interpreter\Html\Section1',
+        'section2'        => '\Hoa\Xyl\Interpreter\Html\Section2',
+        'section3'        => '\Hoa\Xyl\Interpreter\Html\Section3',
+        'section4'        => '\Hoa\Xyl\Interpreter\Html\Section4',
+        'section5'        => '\Hoa\Xyl\Interpreter\Html\Section5',
+        'section6'        => '\Hoa\Xyl\Interpreter\Html\Section6',
+        'tableofcontents' => '\Hoa\Xyl\Interpreter\Html\Tableofcontents',
+        'navigation'      => '\Hoa\Xyl\Interpreter\Html\Navigation',
+        'header'          => '\Hoa\Xyl\Interpreter\Html\Header',
+        'content'         => '\Hoa\Xyl\Interpreter\Html\Content',
+        'footer'          => '\Hoa\Xyl\Interpreter\Html\Footer',
+        'title'           => '\Hoa\Xyl\Interpreter\Html\Title',
+
+        // Grouping content.
+        'p'               => '\Hoa\Xyl\Interpreter\Html\P',
+        'span'            => '\Hoa\Xyl\Interpreter\Html\Span',
+        'ulist'           => '\Hoa\Xyl\Interpreter\Html\Ulist',
+        'olist'           => '\Hoa\Xyl\Interpreter\Html\Olist',
+        'item'            => '\Hoa\Xyl\Interpreter\Html\Item',
+        'error'           => '\Hoa\Xyl\Interpreter\Html\Error',
+
+        // Text-level semantics.
+        'big'             => '\Hoa\Xyl\Interpreter\Html\Big',
+        'em'              => '\Hoa\Xyl\Interpreter\Html\Em',
+        'fbreak'          => '\Hoa\Xyl\Interpreter\Html\Fbreak',
+        'mark'            => '\Hoa\Xyl\Interpreter\Html\Mark',
+        'small'           => '\Hoa\Xyl\Interpreter\Html\Small',
+        'strong'          => '\Hoa\Xyl\Interpreter\Html\Strong',
+        'sub'             => '\Hoa\Xyl\Interpreter\Html\Sub',
+        'sup'             => '\Hoa\Xyl\Interpreter\Html\Sup',
+
+        // Form.
+        'form'            => '\Hoa\Xyl\Interpreter\Html\Form',
+        'input'           => '\Hoa\Xyl\Interpreter\Html\Input',
+        'select'          => '\Hoa\Xyl\Interpreter\Html\Select',
+        'optgroup'        => '\Hoa\Xyl\Interpreter\Html\Optgroup',
+        'option'          => '\Hoa\Xyl\Interpreter\Html\Option',
+        'textarea'        => '\Hoa\Xyl\Interpreter\Html\Textarea',
+        'label'           => '\Hoa\Xyl\Interpreter\Html\Label',
+
+        // Link.
+        'link'            => '\Hoa\Xyl\Interpreter\Html\Link',
+
+        // Phrasing model.
+        '__text'          => '\Hoa\Xyl\Interpreter\Html\Text'
+    );
+
+    /**
+     * Resource path.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html string
+     */
+    protected $_resourcePath = 'Html/Resource/';
+}
+
+}
