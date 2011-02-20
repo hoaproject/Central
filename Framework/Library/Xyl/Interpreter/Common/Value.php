592f7315f5938fa3503f5754e4d41c446a47e223
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-20 13:39:08 +0100

    Add the <value /> component, early draft.

---
+++ Framework/Library/Xyl/Interpreter/Common/Value.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * GNU General Public License
+ *
+ * This file is part of Hoa Open Accessibility.
+ * Copyright (c) 2007, 2011 Ivan ENDERLIN. All rights reserved.
+ *
+ * HOA Open Accessibility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HOA Open Accessibility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HOA Open Accessibility; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Xyl\Element\Concrete
+ */
+-> import('Xyl.Element.Concrete');
+
+}
+
+namespace Hoa\Xyl\Interpreter\Common {
+
+/**
+ * Class \Hoa\Xyl\Interpreter\Common\Value.
+ *
+ * The <value /> component.
+ *
+ * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @license    http://gnu.org/licenses/gpl.txt GNU GPL
+ */
+
+class Value extends \Hoa\Xyl\Element\Concrete {
+
+    /**
+     * Paint the element.
+     *
+     * @access  protected
+     * @param   \Hoa\Stream\IStream\Out  $out    Out stream.
+     * @return  void
+     */
+    public function paint ( \Hoa\Stream\IStream\Out $out ) {
+
+        $this->computeValue($out);
+
+        return;
+    }
+}
+
+}
