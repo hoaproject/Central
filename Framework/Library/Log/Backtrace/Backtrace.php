a16e81594babae2c20c57a0ae122a286454cf1fa
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:59:51 +0100

    Remove ambiguity about copyright: 2007, 2011 to 2007-2011.

--- Framework/Library/Log/Backtrace/Backtrace.php
+++ Framework/Library/Log/Backtrace/Backtrace.php
@@ -65,7 +65,7 @@ namespace Hoa\Log\Backtrace {
  * A DOT output is available.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
