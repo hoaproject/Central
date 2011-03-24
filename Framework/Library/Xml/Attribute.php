7f8b6472b9dda57d01d2609bf62dc697519b9929
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:59:51 +0100

    Remove ambiguity about copyright: 2007, 2011 to 2007-2011.

--- Framework/Library/Xml/Attribute.php
+++ Framework/Library/Xml/Attribute.php
@@ -53,7 +53,7 @@ namespace Hoa\Xml {
  * Parse and manipulate attributes.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
--- Framework/Library/Xml/ReadWrite.php
+++ Framework/Library/Xml/ReadWrite.php
@@ -73,7 +73,7 @@ namespace Hoa\Xml {
  * Read/write a XML element.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
