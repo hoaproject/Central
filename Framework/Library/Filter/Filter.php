2df08e2ddfd4ad0288f176eb3b5594fb06471eaa
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:59:51 +0100

    Remove ambiguity about copyright: 2007, 2011 to 2007-2011.

--- Framework/Library/Filter/Filter.php
+++ Framework/Library/Filter/Filter.php
@@ -60,7 +60,7 @@ import('Factory.~');
  * Build a stack of filter.
  *
  * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright   Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright   Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license     New BSD License
  * @since       PHP 5
  * @version     0.1
