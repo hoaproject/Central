7b35dfdbfb1d00c8def6236559afcdf566eccd27
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:59:51 +0100

    Remove ambiguity about copyright: 2007, 2011 to 2007-2011.

--- Framework/Library/Reflection/Wrapper.php
+++ Framework/Library/Reflection/Wrapper.php
@@ -42,7 +42,7 @@ namespace Hoa\Reflection {
  * Wrap some class and emulate inheritance.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
