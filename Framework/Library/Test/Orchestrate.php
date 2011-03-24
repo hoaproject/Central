2741e2c4fda086fb6f5f5c691c4760855567d113
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 12:06:30 +0100

    Change ENDERLIN to Enderlin '-.-.

--- Framework/Library/Test/Orchestrate.php
+++ Framework/Library/Test/Orchestrate.php
@@ -87,8 +87,8 @@ namespace Hoa\Test {
  *
  * Orchestrate the repository and the code.
  *
- * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2011 Ivan Enderlin.
  * @license    New BSD License
  */
 
--- Framework/Library/Test/Praspel/Constructor/Old.php
+++ Framework/Library/Test/Praspel/Constructor/Old.php
@@ -52,8 +52,8 @@ namespace Hoa\Test\Praspel\Constructor {
  *
  * Represents the \old(e) constructor, the value of e in pre-state.
  *
- * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2011 Ivan Enderlin.
  * @license    New BSD License
  */
 
