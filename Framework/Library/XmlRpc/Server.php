3b2ef4527066918f92c928f6f0a7cd967748f018
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:54:58 +0100

    Change licence clause from GNU GPLv3 to new BSD.

--- Framework/Library/XmlRpc/Server.php
+++ Framework/Library/XmlRpc/Server.php
@@ -67,7 +67,7 @@ import('Xml.~');
  *
  * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
  * @copyright   Copyright (c) 2007, 2011 Ivan ENDERLIN.
- * @license     http://gnu.org/licenses/gpl.txt GNU GPL
+ * @license     New BSD License
  * @since       PHP 5
  * @version     0.2
  * @package     Hoa_XmlRpc
