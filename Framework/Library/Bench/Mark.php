c7ad67f2665ae718dc279e62eda1ea973706c511
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 12:03:09 +0100

    Finally, change (c) for ©, we save 2 bytes ;-).

--- Framework/Library/Bench/Mark.php
+++ Framework/Library/Bench/Mark.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright (c) 2007-2011, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2011, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -54,7 +54,7 @@ namespace Hoa\Bench {
  * Each mark can be start, pause, stop, reset, or compare to an other mark.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
+ * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
