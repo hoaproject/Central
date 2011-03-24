6f0c8bfedfa55a6fd35a1867c9e7379364c5dce1
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:59:51 +0100

    Remove ambiguity about copyright: 2007, 2011 to 2007-2011.

--- Framework/Library/Registry/Registry.php
+++ Framework/Library/Registry/Registry.php
@@ -53,7 +53,7 @@ namespace Hoa\Registry {
  * Hold a register of objects.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
@@ -165,7 +165,7 @@ class Registry extends \ArrayObject {
  * hoa://Library/Registry component.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
