9ab21142cb68b96888b45942444907df550ca418
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 12:03:09 +0100

    Finally, change (c) for ©, we save 2 bytes ;-).

--- Framework/Library/Xyl/Xyl.php
+++ Framework/Library/Xyl/Xyl.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright (c) 2007-2011, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2011, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -83,7 +83,7 @@ namespace Hoa\Xyl {
  * 
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
+ * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
@@ -944,7 +944,7 @@ class          Xyl
  * hoa://Library/Xyl component.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
+ * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
