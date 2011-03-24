c72d0b5d5acd9559a67af2baed2f0f1b2a679787
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 11:59:51 +0100

    Remove ambiguity about copyright: 2007, 2011 to 2007-2011.

--- Framework/Library/Bench/Mark.php
+++ Framework/Library/Bench/Mark.php
@@ -54,7 +54,7 @@ namespace Hoa\Bench {
  * Each mark can be start, pause, stop, reset, or compare to an other mark.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007-2011 Ivan ENDERLIN.
  * @license    New BSD License
  */
 
