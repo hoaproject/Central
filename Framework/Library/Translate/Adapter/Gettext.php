e9b647146515259d35ac6970856cc2eb7ab648df
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 12:06:30 +0100

    Change ENDERLIN to Enderlin '-.-.

--- Framework/Library/Translate/Adapter/Gettext.php
+++ Framework/Library/Translate/Adapter/Gettext.php
@@ -55,8 +55,8 @@ import('File.Read');
  *
  * Gettext adapter (for Unix and Windows).
  *
- * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright   Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author      Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright © 2007-2011 Ivan Enderlin.
  * @license     New BSD License
  * @since       PHP 5
  * @version     0.1
--- Framework/Library/Translate/Translate.php
+++ Framework/Library/Translate/Translate.php
@@ -59,8 +59,8 @@ import('Factory.~');
  *
  * Translate text with different methods.
  *
- * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright   Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author      Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright © 2007-2011 Ivan Enderlin.
  * @license     New BSD License
  * @since       PHP 5
  * @version     0.1
