68954c7bd625820847176a6902a31a4341321973
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 12:06:30 +0100

    Change ENDERLIN to Enderlin '-.-.

--- Framework/Library/Locale/Locale.php
+++ Framework/Library/Locale/Locale.php
@@ -54,8 +54,8 @@ import('Locale.Exception');
  *     * http://unicode.org/cldr/data/common/supplemental/supplementalData.xml
  *     * http://www.iso.org/iso/en/prods-services/iso3166ma/02iso-3166-code-lists/index.html
  *
- * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright   Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author      Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright   Copyright © 2007-2011 Ivan Enderlin.
  * @license     New BSD License
  * @since       PHP 5
  * @version     0.1
