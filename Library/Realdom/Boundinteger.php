2daeac77e0d84fa572ca6bc3f7912b087802fe3b
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-10-21 16:10:58 +0200

    predicate() should use getConstantValue() method because it manipulates only Const* realdom.

--- Library/Realdom/Boundinteger.php
+++ Library/Realdom/Boundinteger.php
@@ -110,8 +110,8 @@ class Boundinteger extends Integer {
     public function predicate ( $q ) {
 
         return    parent::predicate($q)
-               && $q >= $this['lower']->getValue()
-               && $q <= $this['upper']->getValue();
+               && $q >= $this['lower']->getConstantValue()
+               && $q <= $this['upper']->getConstantValue();
     }
 
     /**
