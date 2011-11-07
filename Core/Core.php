5fccb53f78cfad5aeedb140551d8756b183e2904
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-11-07 18:54:51 +0100

    Cancel changeset #1883.

--- Core/Core.php
+++ Core/Core.php
@@ -181,9 +181,6 @@ class Core implements Parameter\Parameterizable {
         if(empty($date))
             ini_set('date.timezone', 'Europe/Paris');
 
-        if(isset($_SERVER['X_HOA_DEBUG']) && true == $_SERVER['X_HOA_DEBUG'])
-            static::startDebugger();
-
         return;
     }
 
@@ -262,6 +259,9 @@ class Core implements Parameter\Parameterizable {
         $this->_parameters->setKeyword('cwd', $cwd);
         $this->_parameters->setParameters($parameters);
         $this->setProtocol();
+
+        if(isset($_SERVER['X_HOA_DEBUG']) && true == $_SERVER['X_HOA_DEBUG'])
+            static::startDebugger();
 
         return $this;
     }
