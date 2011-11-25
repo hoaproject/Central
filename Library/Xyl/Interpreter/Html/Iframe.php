fdab7f5143ed5df657cea5457531fef162c82dd6
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-11-25 11:39:39 +0100

    Add a prototype of <iframe />.

---
+++ Library/Xyl/Interpreter/Html/Iframe.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * New BSD License
+ *
+ * Copyright © 2007-2011, Ivan Enderlin. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hoa nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Xyl\Interpreter\Html\Concrete
+ */
+-> import('Xyl.Interpreter.Html.Concrete')
+
+/**
+ * \Hoa\Xyl\Element\Executable
+ */
+-> import('Xyl.Element.Executable')
+
+/**
+ * \Hoa\Xml\Element\Model\Phrasing
+ */
+-> import('Xml.Element.Model.Phrasing');
+
+}
+
+namespace Hoa\Xyl\Interpreter\Html {
+
+/**
+ * Class \Hoa\Xyl\Interpreter\Html\Iframe.
+ *
+ * The <iframe /> component.
+ *
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2011 Ivan Enderlin.
+ * @license    New BSD License
+ */
+
+class          Iframe
+    extends    Concrete
+    implements \Hoa\Xyl\Element\Executable,
+               \Hoa\Xml\Element\Model\Phrasing {
+
+    /**
+     * Extra attributes.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html\Concrete array
+     */
+    protected $iAttributes = array(
+        'href' => null,
+    );
+
+    /**
+     * Extra attributes mapping.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html\Concrete array
+     */
+    protected $attributesMapping = array(
+        'href' => 'src'
+    );
+
+
+
+    /**
+     * Paint the element.
+     *
+     * @access  protected
+     * @param   \Hoa\Stream\IStream\Out  $out    Out stream.
+     * @return  void
+     */
+    protected function paint ( \Hoa\Stream\IStream\Out $out ) {
+
+        $this->writeAttribute(
+            'src',
+            $this->computeAttributeValue(
+                'href',
+                $this->abstract->readCustomAttributes('href')
+            )
+        );
+        $out->writeAll(
+            '<iframe' . $this->readAttributesAsString() . '></iframe>'
+        );
+
+        return;
+    }
+
+    /**
+     * Pre-execute an element.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function preExecute ( ) {
+
+        return;
+    }
+
+    /**
+     * Post-execute an element.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function postExecute ( ) {
+
+        $this->computeStaticReference();
+        $this->computeHyperReference();
+
+        return;
+    }
+
+    protected function computeHyperReference ( ) {
+
+        if(false === $this->abstract->attributeExists('href'))
+            return;
+
+        $this->abstract->writeAttribute(
+            'href',
+            $this->computeLink($this->abstract->readAttribute('href'))
+        );
+
+        return;
+    }
+
+    protected function computeStaticReference ( ) {
+
+        if(false === $this->abstract->attributeExists('sref'))
+            return;
+
+        $sref   = $this->computeLink($this->abstract->readAttribute('sref'));
+        $this->abstract->writeAttribute('sref', $sref);
+
+        $root   = $this->getAbstractElementSuperRoot();
+        $new    = $root->open($sref);
+        $self   = $new->getConcrete();
+
+        foreach($this->abstract->readCustomAttributes('sref') as $custom => $value) {
+
+            $handle = $new->xpath($value);
+
+            if(!isset($handle[0]))
+                continue;
+
+            $foo    = $self->getConcreteElement($handle[0]);
+            $bar    = trim($foo->computeValue());
+
+            if(empty($bar) && true === $foo->attributeExists('id'))
+                $bar = $foo->readAttribute('id');
+
+            $this->abstract->writeAttribute('href-' . $custom, $bar);
+        }
+
+        return;
+    }
+}
+
+}
