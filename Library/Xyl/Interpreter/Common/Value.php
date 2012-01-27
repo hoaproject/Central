fd1bbad107a50283c7c9660e92145a113aa41a78
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2012-01-27 16:19:46 +0100

    Add @formatter. The @formatter attribute refers to a function name. The @formatter-* attributes refer to arguments name of the refered function. The computeValue() (@bind, textNode etc.) goes in the first undefined arguments.

--- Library/Xyl/Interpreter/Common/Value.php
+++ Library/Xyl/Interpreter/Common/Value.php
@@ -67,6 +67,18 @@ class          Value
     implements \Hoa\Xml\Element\Model\Phrasing {
 
     /**
+     * Attributes description.
+     *
+     * @var \Hoa\Xyl\Interpreter\Html\A array
+     */
+    protected static $_attributes = array(
+        'link'      => self::ATTRIBUTE_TYPE_LINK,
+        'formatter' => self::ATTRIBUTE_TYPE_NORMAL,
+    );
+
+
+
+    /**
      * Paint the element.
      *
      * @access  protected
@@ -74,6 +86,55 @@ class          Value
      * @return  void
      */
     public function paint ( \Hoa\Stream\IStream\Out $out ) {
+
+        $value = $this->computeValue();
+
+        if(true === $this->abstract->attributeExists('formatter')) {
+
+            $formatter   = $this->abstract->readAttribute('formatter');
+            $variables   = $this->abstract->readCustomAttributes('formatter');
+            $self        = $this;
+            array_walk($variables, $f = function ( &$variable ) use ( &$self )  {
+
+                $variable = $self->computeAttributeValue(
+                    $variable,
+                    parent::ATTRIBUTE_TYPE_UNKNOWN
+                );
+
+                if(ctype_digit($variable))
+                    $variable = (int) $variable;
+                elseif(is_numeric($variable))
+                    $variable = (float) $variable;
+                elseif('true' == $variable)
+                    $variable = true;
+                elseif('false' == $variable)
+                    $variable = false;
+                elseif('null' == $variable)
+                    $variable = null;
+            });
+            $reflection  = new \ReflectionFunction($formatter);
+            $arguments   = array();
+            $placeholder = $value;
+            $f($placeholder);
+
+            foreach($reflection->getParameters() as $parameter) {
+
+                $name = strtolower($parameter->getName());
+
+                if(true === array_key_exists($name, $variables)) {
+
+                    $arguments[$name] = $variables[$name];
+                    continue;
+                }
+                elseif(null !== $placeholder) {
+
+                    $arguments[$name] = $placeholder;
+                    $placeholder      = null;
+                }
+            }
+
+            $value = $reflection->invokeArgs($arguments);
+        }
 
         if(true === $this->abstract->attributeExists('link')) {
 
@@ -85,7 +146,7 @@ class          Value
             return;
         }
 
-        $this->computeValue($out);
+        $out->writeAll($value);
 
         return;
     }
