3d4794b54588fc71306df82ee2948a938f6b239f
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2012-08-27 14:50:35 +0200

    Restore Bin/.

---
+++ Bin/Stop.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * New BSD License
+ *
+ * Copyright © 2007-2012, Ivan Enderlin. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hoa nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Worker\Backend\Shared
+ */
+-> import('Worker.Backend.Shared');
+
+}
+
+namespace Hoa\Worker\Bin {
+
+/**
+ * Class \Hoa\Worker\Bin\Stop.
+ *
+ * Stop worker.
+ *
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2012 Ivan Enderlin.
+ * @license    New BSD License
+ */
+
+class Stop extends \Hoa\Console\Dispatcher\Kit {
+
+    /**
+     * Options description.
+     *
+     * @var \Hoa\Worker\Bin\Stop array
+     */
+    protected $options = array(
+        array('run',  \Hoa\Console\GetOption::REQUIRED_ARGUMENT, 'r'),
+        array('help', \Hoa\Console\GetOption::NO_ARGUMENT,       'h'),
+        array('help', \Hoa\Console\GetOption::NO_ARGUMENT,       '?')
+    );
+
+
+
+    /**
+     * The entry method.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function main ( ) {
+
+        $run = 'hoa://Data/Variable/Run/';
+
+        while(false !== $c = $this->getOption($v)) switch($c) {
+
+            case 'r':
+                $run = $v;
+              break;
+
+            case 'h':
+            case '?':
+                return $this->usage();
+              break;
+
+            case '__ambiguous':
+                $this->resolveOptionAmbiguity($v);
+              break;
+        }
+
+        $this->parser->listInputs($workerId);
+
+        if(null === $workerId)
+            return $this->usage();
+
+
+        $run  = resolve($run);
+        \Hoa\Core::getInstance()->initialize(array(
+            'protocol.Data/Variable/Run' => $run . DS
+        ));
+        $password = $this->readPassword('Password: ');
+        $sw       = new \Hoa\Worker\Backend\Shared($workerId, $password);
+        $sw->stop();
+
+        return;
+    }
+
+    /**
+     * The command usage.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function usage ( ) {
+
+        cout('Usage   : worker:stop <options> <worker_id>');
+        cout('Options :');
+        cout($this->makeUsageOptionsList(array(
+            'run'  => 'Define hoa://Data/Variable/Run/ path.',
+            'help' => 'This help.'
+        )));
+
+        return;
+    }
+}
+
+}
