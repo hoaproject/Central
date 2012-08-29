bc3922bf1ca234f6542f93c887c5cdc0cf7f3f11
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2012-08-29 17:59:14 +0200

    Remove “central” keyword from Hoa\Core parameter.
    And, consequently, also the “root.central” parameter.

--- Core.php
+++ Core.php
@@ -213,22 +213,19 @@ class Core implements Parameter\Parameterizable {
      */
     public function initialize ( Array $parameters = array() ) {
 
-        $root              = dirname(dirname(__DIR__));
-        $central           = dirname($root);
-        $cwd               = getcwd() .
-                             ('cli' === PHP_SAPI
-                                 ? DS . dirname($_SERVER['argv'][0])
-                                 : '');
+        $root = dirname(dirname(__DIR__));
+        $cwd  = getcwd() .
+                ('cli' === PHP_SAPI
+                    ? DS . dirname($_SERVER['argv'][0])
+                    : '');
         $this->_parameters = new Parameter(
             $this,
             array(
-                'root'             => $root,
-                'central'          => $central,
-                'cwd'              => $cwd
+                'root' => $root,
+                'cwd'  => $cwd
             ),
             array(
                 'root.hoa'         => '(:root:)',
-                'root.central'     => '(:central:)',
                 'root.application' => '(:cwd:h:)',
                 'root.data'        => '(:%root.application:h:)/Data',
 
@@ -258,9 +255,8 @@ class Core implements Parameter\Parameterizable {
             )
         );
 
-        $this->_parameters->setKeyword('root',    $root);
-        $this->_parameters->setKeyword('central', $central);
-        $this->_parameters->setKeyword('cwd',     $cwd);
+        $this->_parameters->setKeyword('root', $root);
+        $this->_parameters->setKeyword('cwd',  $cwd);
         $this->_parameters->setParameters($parameters);
         $this->setProtocol();
 
