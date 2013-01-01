812fc237b3a779bddf50ae6c1bb3c501d7a272b9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-01-01 13:20:35 +0100

    Happy new year \o/.

--- Dns.php
+++ Dns.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2012, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2013, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -54,7 +54,7 @@ namespace Hoa\Dns {
  * Please, see RFC6195, RFC1035 and RFC1034 for an overview.
  *
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2012 Ivan Enderlin.
+ * @copyright  Copyright © 2007-2013 Ivan Enderlin.
  * @license    New BSD License
  */
 
