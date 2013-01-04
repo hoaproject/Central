eaa388e022962ee6b639b2fb65c1212c15f93a4b
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-01-04 01:18:07 +0100

    Update API documentation.

--- Grammar.pp
+++ Grammar.pp
@@ -31,7 +31,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
-// Grammar \Hoa\Test\Praspel\Grammar.
+// Grammar \Hoa\Praspel\Grammar.
 //
 // Provide grammar for Praspel.
 //
