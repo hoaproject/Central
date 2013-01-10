7a13070c05763f91c2611fc35eb09a14e33e5327
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2013-01-10 23:40:08 +0100

    Add setOutputStream method.

--- Xyl.php
+++ Xyl.php
@@ -383,6 +383,21 @@ class          Xyl
     }
 
     /**
+     * Set output stream.
+     *
+     * @access  public
+     * @param   \Hoa\Stream\IStream\Out  $out   Output stream.
+     * @return  \Hoa\Stream\IStream\Out
+     */
+    public function setOutputStream ( \Hoa\Stream\IStream\Out $out ) {
+
+      $old = $this->_out;
+      $this->_out = $out;
+
+      return $old;
+    }
+
+    /**
      * Get output stream.
      *
      * @access  public
