449659e3f10a2a1ee13239a0eb007e4f29c172a0
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-06-25 14:27:11 +0200

    Add the Hoa\Core::uuid() static method.
    Moved from `hoa core:uuid`.

--- Core.php
+++ Core.php
@@ -388,6 +388,27 @@ class Core implements Parameter\Parameterizable {
     }
 
     /**
+     * Generate an Universal Unique Identifier (UUID).
+     *
+     * @access  public
+     * @return  void
+     */
+    public static function uuid ( ) {
+
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
+    }
+
+    /**
      * Return the copyright and license of Hoa.
      *
      * @access  public
