0c2c75d01ce0a844344ef8d4abb0f2cc42c6c6ec
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-11-25 15:11:33 +0100

    Update indexes of exceptions.

--- Model/Behavior.php
+++ Model/Behavior.php
@@ -232,7 +232,7 @@ class Behavior extends Clause {
             default:
                 throw new \Hoa\Praspel\Exception\Model(
                     'Clause @%s is unknown.',
-                    1, array($clause, $this->getName()));
+                    6, array($clause, $this->getName()));
         }
 
         return $this->_clauses[$clause] = $handle;
@@ -252,7 +252,7 @@ class Behavior extends Clause {
         if(false === in_array($name, static::getAllowedClauses()))
             throw new \Hoa\Praspel\Exception\Model(
                 'Clause @%s is not allowed in @%s.',
-                0, array($name, $this->getId()));
+                7, array($name, $this->getId()));
 
         $clause->setParent($this);
 
