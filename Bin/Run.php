70ebae41f408c2343cb69578073c5ab6e2263f35
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-05-07 18:19:34 +0200

    First draft.

---
+++ Bin/Run.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * New BSD License
+ *
+ * Copyright © 2007-2014, Ivan Enderlin. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hoa nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace {
+
+from('Hoa')
+
+/**
+ * \Hoa\Console\Processus
+ */
+-> import('Console.Processus');
+
+}
+
+namespace Hoa\Test\Bin {
+
+/**
+ * Class Hoa\Test\Bin\Run.
+ *
+ * Run tests.
+ *
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2014 Ivan Enderlin.
+ * @license    New BSD License
+ */
+
+class Run extends \Hoa\Console\Dispatcher\Kit {
+
+    /**
+     * Options description.
+     *
+     * @var \Hoa\Test\Bin\Pp array
+     */
+    protected $options = array(
+        array('library',   \Hoa\Console\GetOption::REQUIRED_ARGUMENT, 'l'),
+        array('directory', \Hoa\Console\GetOption::REQUIRED_ARGUMENT, 'd'),
+        array('file',      \Hoa\Console\GetOption::REQUIRED_ARGUMENT, 'f'),
+        array('help',      \Hoa\Console\GetOption::NO_ARGUMENT,       'h'),
+        array('help',      \Hoa\Console\GetOption::NO_ARGUMENT,       '?')
+    );
+
+
+
+    /**
+     * The entry method.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function main ( ) {
+
+        $library   = null;
+        $directory = null;
+        $file      = null;
+
+        while(false !== $c = $this->getOption($v)) switch($c) {
+
+            case 'l':
+                $library = ucfirst(strtolower($v));
+              break;
+
+            case 'd':
+                $directory = $v;
+              break;
+
+            case 'f':
+                $file = $v;
+              break;
+
+            case '__ambiguous':
+                $this->resolveOptionAmbiguity($v);
+              break;
+
+            case 'h':
+            case '?':
+            default:
+                return $this->usage();
+              break;
+        }
+
+        $command = 'atoum' .
+                   ' --bootstrap-file ' .
+                       resolve('hoa://Library/Test/.bootstrap.atoum.php') .
+                   ' --force-terminal';
+
+        if(null !== $library) {
+
+            $command .= ' --directories ' . resolve('hoa://Library/' . $library);
+        }
+        elseif(null !== $directory)
+            $command .= ' --directories ' . $directory;
+        elseif(null !== $file)
+            $command .= ' --files ' . $file;
+
+        $processus = new \Hoa\Console\Processus($command);
+        $processus->on('input', function ( $bucket ) {
+
+            return false;
+        });
+        $processus->on('output', function ( $bucket ) {
+
+            $data = $bucket->getData();
+
+            echo $data['line'], "\n";
+
+            return;
+        });
+        $processus->run();
+
+        return;
+    }
+
+    /**
+     * The command usage.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function usage ( ) {
+
+        echo 'Usage   : test:run <options>', "\n",
+             'Options :', "\n",
+             $this->makeUsageOptionsList(array(
+                 'l'    => 'Run all tests of a library.',
+                 'd'    => 'Run tests of a specific directory.',
+                 'f'    => 'Run test of a specific file.',
+                 'help' => 'This help.'
+             )), "\n";
+
+        return;
+    }
+}
+
+}
+
+__halt_compiler();
+Run tests.
---
+++ README.md
@@ -0,0 +1,52 @@
+![Hoa](http://static.hoa-project.net/Image/Hoa_small.png)
+
+Hoa is a **modular**, **extensible** and **structured** set of PHP libraries.
+Moreover, Hoa aims at being a bridge between industrial and research worlds.
+
+# Hoa\Test ![state](http://central.hoa-project.net/State/Test)
+
+This library provides tools to create and run tests of Hoa.
+
+In each library, a `Test/` directory contains test suites. So far, only unit
+tests are supported. They are written with [atoum](http://atoum.org/).
+
+## Quick usage
+
+As a quick overview, we see how to write and execute a unit test for the
+`Hoa\Foo` library.
+
+First, let's create the `Hoa/Foo/Test/Unit/Bar.php` file, that contains:
+
+```php
+namespace Hoa\Foo\Test\Unit {
+
+class Bar extends \Hoa\Test\Unit\Suite {
+
+    public function caseBaz ( ) {
+
+        $this->integer(7 * 3 * 2)->isEqualTo(42);
+    }
+}
+
+}
+```
+
+A class represents a test suite (that extends the `Hoa\Test\Unit\Suite` class).
+A method represents a test case, where its name must be prefixed by `case`.
+
+Second, and last, we will use the `hoa test:run` script to execute this test. We
+will execute all the test suites of a library with the `--library` option. Thus:
+
+```sh
+$ hoa test:run --library Foo
+```
+
+## Documentation
+
+Different documentations can be found on the website:
+[http://hoa-project.net/](http://hoa-project.net/).
+
+## License
+
+Hoa is under the New BSD License (BSD-3-Clause). Please, see
+[`LICENSE`](http://hoa-project.net/LICENSE).
