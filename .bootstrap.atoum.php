34015b0b23795ac688a657dc74719017699eb498
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-05-08 15:04:50 +0200

    Extract atoum's autoloader in a specific file.

--- .bootstrap.atoum.php
+++ .bootstrap.atoum.php
@@ -1,20 +1,3 @@
 <?php
 
-$composer = dirname(__DIR__) . DIRECTORY_SEPARATOR .
-            '..' . DIRECTORY_SEPARATOR .
-            '..' . DIRECTORY_SEPARATOR .
-            '..' . DIRECTORY_SEPARATOR .
-            'autoload.php';
-
-if(file_exists($composer))
-    require_once $composer;
-else
-    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR .
-                 'Core' . DIRECTORY_SEPARATOR .
-                 'Core.php';
-
-if(isset($_SERVER['HOA_ATOUM_PRASPEL_EXTENSION']))
-    \Hoa\Core::getInstance()->getParameters()->setParameter(
-        'namespace.prefix.Atoum',
-        $_SERVER['HOA_ATOUM_PRASPEL_EXTENSION']
-    );
+require_once __DIR__ . DIRECTORY_SEPARATOR . '.autoload.atoum.php';
