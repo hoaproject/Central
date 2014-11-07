8375df5d53e2c48cae1606880610394b957d7339
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-11-07 16:06:49 +0100

    Start.

---
+++ Bin/State.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * New BSD License
+ *
+ * Copyright © 2007-2014, Ivan Enderlin. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hoa nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Hoa\Devtools\Bin;
+
+use Hoa\Console;
+
+/**
+ * Class \Hoa\Devtools\Bin\State.
+ *
+ * Get state of a library.
+ *
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2014 Ivan Enderlin.
+ * @license    New BSD License
+ */
+
+class State extends Console\Dispatcher\Kit {
+
+    /**
+     * Options description.
+     *
+     * @var \Hoa\Devtools\Bin\State array
+     */
+    protected $options = [
+        ['help', Console\GetOption::NO_ARGUMENT, 'h'],
+        ['help', Console\GetOption::NO_ARGUMENT, '?']
+    ];
+
+
+
+    /**
+     * The entry method.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function main ( ) {
+
+        $library = null;
+
+        while(false !== $c = $this->getOption($v)) switch($c) {
+
+            case '__ambiguous':
+                $this->resolveOptionAmbiguity($v);
+              break;
+
+            case 'h':
+            case '?':
+            default:
+                return $this->usage();
+              break;
+        }
+
+        $this->parser->listInputs($library);
+
+        if(empty($library))
+            return $this->usage();
+
+        $library = ucfirst(strtolower($library));
+        $path    = 'hoa://Library/' . $library;
+
+        if(false === file_exists($path))
+            throw new Console\Exception(
+                'The %s library does not exist.',
+                0, $library);
+
+        $status  = 'beta';
+        $path   .= '/.State';
+
+        if(true === file_exists($path))
+            $status = trim(file_get_contents($path));
+
+        echo $status;
+
+        return;
+    }
+
+    /**
+     * The command usage.
+     *
+     * @access  public
+     * @return  int
+     */
+    public function usage ( ) {
+
+        echo 'Usage   : devtools:state <options> library', "\n",
+             'Options :', "\n",
+             $this->makeUsageOptionsList([
+                 'help' => 'This help.'
+             ]), "\n";
+
+        return;
+    }
+}
+
+__halt_compiler();
+Get the state of a library.
---
+++ README.md
@@ -0,0 +1,39 @@
+![Hoa](http://static.hoa-project.net/Image/Hoa_small.png)
+
+Hoa is a **modular**, **extensible** and **structured** set of PHP libraries.
+Moreover, Hoa aims at being a bridge between industrial and research worlds.
+
+# Hoa\Devtools ![state](http://central.hoa-project.net/State/Devtools)
+
+This library contains several development tools. This is for developers or
+maintainers. Sometimes it can be useful to users also, but in particular cases.
+
+## Installation
+
+With [Composer](http://getcomposer.org/), to include this library into your
+dependencies, you need to require
+[`hoa/devtools`](https://packagist.org/packages/hoa/devtools):
+
+```json
+{
+    "require": {
+        "hoa/devtools": "~0.0"
+    }
+}
+```
+
+Please, read the website to [get more informations about how to
+install](http://hoa-project.net/Source.html).
+
+## Quick usage
+
+
+## Documentation
+
+Different documentations can be found on the website:
+[http://hoa-project.net/](http://hoa-project.net/).
+
+## License
+
+Hoa is under the New BSD License (BSD-3-Clause). Please, see
+[`LICENSE`](http://hoa-project.net/LICENSE).
