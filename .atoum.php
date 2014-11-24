b232ad1f06555cb0bf68df2a12b9230f0b3b19c2
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-11-24 10:49:32 +0100

    Format code. #mania

--- .atoum.php
+++ .atoum.php
@@ -2,4 +2,4 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '.autoload.atoum.php';
 
-$runner->addExtension(new \Atoum\PraspelExtension\Manifest());
+$runner->addExtension(new Atoum\PraspelExtension\Manifest());
