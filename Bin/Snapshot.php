96c08e32933637eb2c948a779c709819ce6a4dc4
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-09 21:09:03 +0100

    New workflow.
    This assistant drives us through different steps.

--- Bin/Snapshot.php
+++ Bin/Snapshot.php
@@ -41,7 +41,7 @@ use Hoa\Console;
 /**
  * Class \Hoa\Devtools\Bin\Snapshot.
  *
- * Snapshot and generate changelog for the current repository root.
+ * Assistant to create a snapshot.
  *
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
  * @copyright  Copyright © 2007-2015 Ivan Enderlin.
@@ -56,10 +56,9 @@ class Snapshot extends Console\Dispatcher\Kit {
      * @var \Hoa\Devtools\Bin\Requiresnapshot array
      */
     protected $options = [
-        ['break-bc',       Console\GetOption::NO_ARGUMENT, 'b'],
-        ['only-changelog', Console\GetOption::NO_ARGUMENT, 'c'],
-        ['help',           Console\GetOption::NO_ARGUMENT, 'h'],
-        ['help',           Console\GetOption::NO_ARGUMENT, '?']
+        ['break-bc', Console\GetOption::NO_ARGUMENT, 'b'],
+        ['help',     Console\GetOption::NO_ARGUMENT, 'h'],
+        ['help',     Console\GetOption::NO_ARGUMENT, '?']
     ];
 
 
@@ -72,8 +71,7 @@ class Snapshot extends Console\Dispatcher\Kit {
      */
     public function main ( ) {
 
-        $breakBC       = false;
-        $onlyChangelog = false;
+        $breakBC = false;
 
         while(false !== $c = $this->getOption($v)) switch($c) {
 
@@ -83,10 +81,6 @@ class Snapshot extends Console\Dispatcher\Kit {
 
             case 'b':
                 $breakBC = $v;
-              break;
-
-            case 'c':
-                $onlyChangelog = $v;
               break;
 
             case 'h':
@@ -121,54 +115,241 @@ class Snapshot extends Console\Dispatcher\Kit {
             ++$currentMCN;
 
         $newTag = $currentMCN . '.' . date('y.m.d');
-        $steps  = $tags;
 
-        if(false === $onlyChangelog) {
+        echo 'We are going to release this library together, by following ',
+             'these steps:', "\n",
+             '  1. tests must pass,', "\n",
+             '  2. updating the CHANGELOG.md file,', "\n",
+             '  3. commit the CHANGELOG.md file,', "\n",
+             '  4. creating a tag,', "\n",
+             '  5. pushing the tag,', "\n",
+             '  6. creating a release on Github.', "\n";
 
-            $answer = $this->readLine(
-                'Create tag ' . $newTag . ' for repository ' .
-                realpath($repositoryRoot) . '? [yes/no] '
-            );
+        $step = function ( $message, $task ) {
+
+            echo "\n\n";
+            Console\Cursor::colorize('foreground(black) background(yellow)');
+            echo 'Step “', $message, '”.';
+            Console\Cursor::colorize('normal');
+            echo "\n";
+
+            $answer = $this->readLine('Would you like to do this one: [yes/no] ');
 
             if('yes' === $answer) {
+
+                echo "\n";
+                $task();
+            }
+            else {
+
+                Console\Cursor::colorize('foreground(red)');
+                echo 'Aborted!', "\n";
+                Console\Cursor::colorize('normal');
+            }
+        };
+
+        $step(
+            'tests must pass',
+            function ( ) {
+
+                echo 'Tests must be green. Execute:', "\n",
+                     '    $ hoa test:run -d Test', "\n",
+                     'to run the tests.', "\n";
+                $this->readLine('Press Enter when it is green.');
+            }
+        );
+
+        $step(
+            'updating the CHANGELOG.md file',
+            function ( ) use ( $tags, $repositoryRoot ) {
+
+                $changelog = null;
+
+                for($i = 0, $max = count($tags) - 1; $i < $max; ++$i) {
+
+                    $fromStep = $tags[$i];
+                    $toStep   = $tags[$i + 1];
+
+                    $changelog .= '# ' . '`' . $fromStep . '`' . "\n\n" .
+                                  Console\Processus::execute(
+                                      'git --git-dir=' . $repositoryRoot . '/.git ' .
+                                          'log ' .
+                                              '--first-parent ' .
+                                              '--pretty="format:  * %h %s (%aN, %aI)" ' .
+                                              $fromStep . '...' . $toStep,
+                                      false
+                                  ) . "\n\n";
+
+                    if($max < $i + 2)
+                        $changelog .=  $toStep . "\n\n" .
+                                      '(first snapshot)';
+                }
+
+                echo $changelog;
+
+                return;
+            }
+        );
+
+        $step(
+            'commit the CHANGELOG.md file',
+            function ( ) {
+
+                echo 'Great! Now commit the CHANGELOG.md file!', "\n";
+                $this->readLine('Press Enter when it is done.');
+
+                return;
+            }
+        );
+
+        $step(
+            'creating a tag',
+            function ( ) use ( $breakBC, $step, $currentMCN, $repositoryRoot,
+                               $tags, $newTag ) {
+
+                if(true === $breakBC) {
+
+                    echo 'A BC break has been introduced, ',
+                         'few more steps are required:', "\n";
+
+                    $step(
+                        'update the composer.json file',
+                        function ( ) use ( $currentMCN ) {
+
+                            echo 'The `extra.branch-alias.dev-master` value ',
+                                 'must be set to `',
+                                 $currentMCN, '.x-dev`', "\n";
+                            $this->readLine('Press Enter when it is done.');
+                        }
+                    );
+
+                    $step(
+                        'open issues to update parent dependencies',
+                        function ( ) {
+
+                            echo 'Some libraries may depend on this one. ',
+                                 'Issues must be opened to update this ',
+                                 'dependency.', "\n";
+                            $this->readLine('Press Enter when it is done.');
+                        }
+                    );
+
+                    $step(
+                        'update the README.md file',
+                        function ( ) use ( $currentMCN ) {
+
+                            echo 'The installation Section must invite the ',
+                                 'user to install the version ',
+                                 '`~', $currentMCN, '.0`.', "\n";
+                            $this->readLine('Press Enter when it is done.');
+                        }
+                    );
+
+                    $step(
+                        'commit the composer.json and README.md files',
+                        function ( ) use ( $currentMCN ) {
+
+                            echo 'Great! Now commit the composer.json and ',
+                                 'README.md files!', "\n";
+                            $this->readLine('Press Enter when it is done.');
+                        }
+                    );
+                }
+
+                $status = Console\Processus::execute(
+                    'git --git-dir=' . $repositoryRoot . '/.git ' .
+                        'status ' .
+                            '--short'
+                );
+
+                if(!empty($status)) {
+
+                    Console\Cursor::colorize('foreground(white) background(red)');
+                    echo 'At least one file is not commited!';
+                    Console\Cursor::colorize('normal');
+                    echo "\n", '(tips: use `git stash` if it is not related ',
+                         'to this snapshot)', "\n";
+
+                    $this->readLine('Press Enter when everything is clean.');
+                }
+
+                echo 'Here is the list of tags:', "\n",
+                     '  * ', implode(',' . "\n" . '  * ', $tags), '.', "\n",
+                     'We are going to create the following tag: ',
+                     $newTag, '.', "\n";
+
+                $answer = $this->readLine('Is it correct? [yes/no] ');
+
+                if('yes' !== $answer)
+                    return;
 
                 Console\Processus::execute(
                     'git --git-dir=' . $repositoryRoot . '/.git ' .
                         'tag ' . $newTag
                 );
-                echo 'Done!', "\n";
-
-                array_unshift($steps, $newTag);
             }
-            else
-                echo 'Aborted!', "\n";
-        }
-        else
-            array_unshift($steps, 'HEAD');
+        );
 
-        $changelog = null;
+        $step(
+            'push the new snapshot',
+            function ( ) use ( $repositoryRoot ) {
 
-        for($i = 0, $max = count($steps) - 1; $i < $max; ++$i) {
+                Console\Cursor::colorize('foreground(white) background(red)');
+                echo 'This step ',
+                Console\Cursor::colorize('underlined');
+                echo 'must not';
+                Console\Cursor::colorize('!underlined');
+                echo ' be undo!';
+                Console\Cursor::colorize('normal');
 
-            $fromStep = $steps[$i];
-            $toStep   = $steps[$i + 1];
+                echo "\n";
 
-            $changelog .= '# ' . '`' . $fromStep . '`' . "\n\n" .
-                          Console\Processus::execute(
-                              'git --git-dir=' . $repositoryRoot . '/.git ' .
-                                  'log ' .
-                                      '--first-parent ' .
-                                      '--pretty="format:  * %h %s (%aN, %aI)" ' .
-                                      $fromStep . '...' . $toStep,
-                              false
-                          ) . "\n\n";
+                $i = 5;
+                while($i-- > 0) {
 
-            if($max < $i + 2)
-                $changelog .= '`' . $toStep . '`' . "\n\n" .
-                              '(first snapshot)';
-        }
+                    Console\Cursor::clear('↔');
+                    echo ($i + 1);
+                    sleep(1);
+                }
 
-        echo "\n", $changelog;
+                $remotes = Console\Processus::execute(
+                    'git --git-dir=' . $repositoryRoot . '/.git ' .
+                        'remote ' .
+                            '--verbose'
+                );
+
+                $gotcha = false;
+
+                foreach(explode("\n", $remotes) as $remote)
+                    if(0 !== preg_match('/(git@git.hoa-project.net:[^ ]+)/', $remote, $matches)) {
+
+                        $gotcha = true;
+
+                        break;
+                    }
+
+                if(false === $gotcha) {
+
+                    echo 'No remote has been found.';
+
+                    return;
+                }
+
+                echo "\n", 'To push tag, execute:', "\n",
+                     '    $ git push ', $matches[1], "\n";
+                $this->readLine('Press Enter when it is done.');
+            }
+        );
+
+        $step(
+            'create a release on Github',
+            function ( ) {
+
+                
+            }
+        );
+
+        echo "\n", '🍺 🍺 🍺', "\n";
 
         return;
     }
@@ -186,7 +367,6 @@ class Snapshot extends Console\Dispatcher\Kit {
              $this->makeUsageOptionsList([
                  'b'    => 'Whether we have break the backward compatibility ' .
                            'or not.',
-                 'c'    => 'Only print the changelog.',
                  'help' => 'This help.'
              ]), "\n";
 
@@ -195,4 +375,4 @@ class Snapshot extends Console\Dispatcher\Kit {
 }
 
 __halt_compiler();
-Snapshot and generate changelog for the current repository root.
+Assistant to create a snapshot.
