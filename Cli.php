0a0ca46899138ce3d79bfd9976801bbd5066108a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-05-20 09:27:10 +0200

    Move to PSR-1 and PSR-2.

--- Cli.php
+++ Cli.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2015, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -43,24 +43,22 @@ use Hoa\Core;
  *
  * CLI router.
  *
- * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2015 Ivan Enderlin.
+ * @copyright  Copyright © 2007-2015 Hoa community
  * @license    New BSD License
  */
-
-class Cli extends Generic implements Core\Parameter\Parameterizable {
-
+class Cli extends Generic implements Core\Parameter\Parameterizable
+{
     /**
      * Parameters.
      *
-     * @var \Hoa\Core\Parameter object
+     * @var \Hoa\Core\Parameter
      */
     protected $_parameters     = null;
 
     /**
      * CLI methods that the router understand.
      *
-     * @var \Hoa\Router\Http array
+     * @var array
      */
     protected static $_methods = [
         'get'
@@ -71,11 +69,10 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
     /**
      * Constructor.
      *
-     * @access  public
      * @return  void
      */
-    public function __construct ( Array $parameters = [] ) {
-
+    public function __construct(Array $parameters = [])
+    {
         $this->_parameters = new Core\Parameter(
             $this,
             [],
@@ -86,24 +83,24 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
         );
         $this->_parameters->setParameters($parameters);
 
-        foreach($this->_parameters->getParameter('rules.public') as $id => $rule) {
-
+        foreach ($this->_parameters->getParameter('rules.public') as $id => $rule) {
             @list($methods, $pattern, $call, $able, $variables)
                 = $rule;
 
-            if(null === $variables)
+            if (null === $variables) {
                 $variables = [];
+            }
 
             $this->addRule($id, $methods, $pattern, $call, $able, $variables);
         }
 
-        foreach($this->_parameters->getParameter('rules.private') as $id => $rule) {
-
+        foreach ($this->_parameters->getParameter('rules.private') as $id => $rule) {
             @list($methods, $pattern, $call, $able, $variables)
                 = $rule;
 
-            if(null === $variables)
+            if (null === $variables) {
                 $variables = [];
+            }
 
             $this->addPrivateRule(
                 $id, $methods, $pattern, $call, $able, $variables
@@ -116,18 +113,16 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
     /**
      * Get parameters.
      *
-     * @access  public
      * @return  \Hoa\Core\Parameter
      */
-    public function getParameters ( ) {
-
+    public function getParameters()
+    {
         return $this->_parameters;
     }
 
     /**
      * Fallback for add*Rule() methods.
      *
-     * @access  public
      * @param   int     $visibility    Visibility (please, see
      *                                 Router::VISIBILITY_* constants).
      * @param   string  $id            ID.
@@ -137,28 +132,44 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
      * @param   mixed   $able          Able (second part).
      * @param   array   $variables     Variables (default or additional values).
      * @return  \Hoa\Router\Http
-     * @throw   \Hoa\Router\Exception
+     * @throws  \Hoa\Router\Exception
      */
-    protected function _addRule ( $visibility, $id, Array $methods, $pattern,
-                                  $call, $able, Array $variables ) {
-
-        if(true === $this->ruleExists($id))
+    protected function _addRule(
+        $visibility,
+        $id,
+        Array $methods,
+        $pattern,
+        $call, $able,
+        Array $variables
+    ) {
+        if (true === $this->ruleExists($id)) {
             throw new Exception(
-                'Cannot add rule %s because it already exists.', 0, $id);
+                'Cannot add rule %s because it already exists.',
+                0,
+                $id
+            );
+        }
 
-        array_walk($methods, function ( &$method ) {
+        array_walk($methods, function (&$method) {
 
             $method = strtolower($method);
         });
         $diff = array_diff($methods, self::$_methods);
 
-        if(!empty($diff))
+        if (!empty($diff)) {
             throw new Exception(
                 (1 == count($diff)
                     ? 'Method %s is'
                     : 'Methods %s are') .
                 ' invalid for the rule %s (valid methods are: %s).',
-                1, [implode(', ', $diff), $id, implode(', ', self::$_methods)]);
+                1,
+                [
+                    implode(', ', $diff),
+                    $id,
+                    implode(', ', self::$_methods)
+                ]
+            );
+        }
 
         $this->_rules[$id] = [
             Router::RULE_VISIBILITY => $visibility,
@@ -177,26 +188,27 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
      * Find the appropriated rule.
      * Special variables: _call, _able and _tail.
      *
-     * @access  public
      * @param   string  $uri     URI. If null, it will be deduce.
      * @return  \Hoa\Router\Cli
-     * @throw   \Hoa\Router\Exception\NotFound
+     * @throws  \Hoa\Router\Exception\NotFound
      */
-    public function route ( $uri = null ) {
-
-        if(null === $uri)
+    public function route($uri = null)
+    {
+        if (null === $uri) {
             $uri = static::getURI();
+        }
 
         $method = $this->getMethod();
         $rules  = array_filter(
             $this->getRules(),
-            function ( $rule ) use ( &$method ) {
-
-                if(Router::VISIBILITY_PUBLIC != $rule[Router::RULE_VISIBILITY])
+            function ($rule) use (&$method) {
+                if (Router::VISIBILITY_PUBLIC != $rule[Router::RULE_VISIBILITY]) {
                     return false;
+                }
 
-                if(false === in_array($method, $rule[Router::RULE_METHODS]))
+                if (false === in_array($method, $rule[Router::RULE_METHODS])) {
                     return false;
+                }
 
                 return true;
             }
@@ -204,20 +216,23 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
 
         $gotcha = false;
 
-        foreach($rules as $rule) {
-
+        foreach ($rules as $rule) {
             $pattern = $rule[Router::RULE_PATTERN];
 
-            if(0 !== preg_match('#^' . $pattern . '$#i', $uri, $muri)) {
-
+            if (0 !== preg_match('#^' . $pattern . '$#i', $uri, $muri)) {
                 $gotcha = true;
+
                 break;
             }
         }
 
-        if(false === $gotcha)
+        if (false === $gotcha) {
             throw new Exception\NotFound(
-                'Cannot found an appropriated rule to route %s.', 5, $uri);
+                'Cannot found an appropriated rule to route %s.',
+                2,
+                $uri
+            );
+        }
 
         array_shift($muri);
         $rule[Router::RULE_VARIABLES]['_method'] = 'get';
@@ -229,16 +244,18 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
             $rule[Router::RULE_PATTERN]
         );
 
-        foreach($muri as $key => $value) {
-
-            if(!is_string($key))
+        foreach ($muri as $key => $value) {
+            if (!is_string($key)) {
                 continue;
+            }
 
-            if(true === $caseless)
+            if (true === $caseless) {
                 $key = mb_strtolower($key);
+            }
 
-            if(isset($rule[Router::RULE_VARIABLES][$key]) && empty($value))
+            if (isset($rule[Router::RULE_VARIABLES][$key]) && empty($value)) {
                 continue;
+            }
 
             $rule[Router::RULE_VARIABLES][$key] = $value;
         }
@@ -251,24 +268,23 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
     /**
      * Unroute a rule (i.e. route()^-1).
      *
-     * @access  public
      * @param   string  $id           ID.
      * @param   array   $variables    Variables.
      * @return  string
      */
-    public function unroute ( $id, Array $variables = [] ) {
-
+    public function unroute($id, Array $variables = [])
+    {
         $rule      = $this->getRule($id);
         $pattern   = $rule[Router::RULE_PATTERN];
         $variables = array_merge($rule[Router::RULE_VARIABLES], $variables);
         $out       = preg_replace_callback(
             '#\(\?\<([^>]+)>[^\)]*\)[\?\*\+]{0,2}#',
-            function ( Array $matches ) use ( &$variables ) {
-
+            function (Array $matches) use (&$variables) {
                 $m = strtolower($matches[1]);
 
-                if(!isset($variables[$m]))
+                if (!isset($variables[$m])) {
                     return '';
+                }
 
                 return $variables[$m];
             },
@@ -291,65 +307,62 @@ class Cli extends Generic implements Core\Parameter\Parameterizable {
     /**
      * Get HTTP method.
      *
-     * @access  public
      * @return  string
      */
-    public function getMethod ( ) {
-
+    public function getMethod()
+    {
         return 'get';
     }
 
     /**
      * Whether the router is called asynchronously or not.
      *
-     * @access  public
      * @return  bool
      */
-    public function isAsynchronous ( ) {
-
+    public function isAsynchronous()
+    {
         return false;
     }
 
     /**
      * Get URI.
      *
-     * @access  public
      * @return  string
      */
-    public static function getURI ( ) {
-
-        if(!isset($_SERVER['argv']))
+    public static function getURI()
+    {
+        if (!isset($_SERVER['argv'])) {
             return null;
+        }
 
         $out   = null;
         $_argv = $_SERVER['argv'];
         array_shift($_argv);
 
-        foreach($_argv as $arg) {
-
-            if('-' === $arg[0] && false !== strpos($arg, '=')) {
-
-                if(false !== strpos($arg, '"'))
+        foreach ($_argv as $arg) {
+            if ('-' === $arg[0] && false !== strpos($arg, '=')) {
+                if (false !== strpos($arg, '"')) {
                     $arg = str_replace(
                         '=',
                         '="',
                         str_replace('"', '\\"', $arg)
                     ) . '"';
-                elseif(false !== strpos($arg, '\''))
+                } elseif (false !== strpos($arg, '\'')) {
                     $arg = str_replace(
                         '=',
                         '=\'',
                         str_replace('\'', '\\\'', $arg)
                     ) . '\'';
-                elseif(false !== strpos($arg, ' '))
+                } elseif (false !== strpos($arg, ' ')) {
                     $arg = str_replace('=', '="', $arg) . '"';
-            }
-            elseif(false !== strpos($arg, ' '))
+                }
+            } elseif (false !== strpos($arg, ' ')) {
                 $arg = '"' . str_replace('"', '\\"', $arg) . '"';
-            elseif(false !== strpos($arg, '"'))
+            } elseif (false !== strpos($arg, '"')) {
                 $arg = '"' . str_replace('"', '\\"', $arg) . '"';
-            elseif(false !== strpos($arg, '\''))
+            } elseif (false !== strpos($arg, '\'')) {
                 $arg = '"' . $arg . '"';
+            }
 
             $out .= ' ' . $arg;
         }
