9e474c81da278ffae5f175490e6acbd3cb40b69b
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-09-01 12:03:27 +0200

    Force `atoum` to run in a specific CWD.
    The current working directory (CWD) of atoum was not very well defined.
    Now we force it to `hoa://Library/Test/`. In this case, it is able to
    find a default `.bootstrap.atoum.php` file, and then it can load it to
    find command-line arguments to add (useful for the
    `atoum/ruler-extension` extension). We set a `HOA_PREVIOUS_CWD`
    environment variable to reset the CWD before executing the tests. Why?
    Because, for instance, if we execute:
    
        $ vendor/bin/hoa test:run --directories Test/
    
    With CWD set to `hoa://Library/Test/`, the `Test/` directory will not
    exist, because it will look for `hoa://Library/Test/Test`. By resetting
    the CWD, we fix this problem (back to normal).

--- .bootstrap.atoum.php
+++ .bootstrap.atoum.php
@@ -1,5 +1,9 @@
 <?php
 
+if (isset($_SERVER['HOA_PREVIOUS_CWD'])) {
+    chdir($_SERVER['HOA_PREVIOUS_CWD']);
+}
+
 require_once __DIR__ . DIRECTORY_SEPARATOR . '.autoload.atoum.php';
 
 Hoa\Core::enableErrorHandler();
