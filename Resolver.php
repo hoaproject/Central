c778e2b9085e1cad5228e7111f9a8f05b401dd38
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-01-09 09:00:51 +0100

    Quality: Run devtools:cs.

--- Resolver.php
+++ Resolver.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Hoa community. All rights reserved.
+ * Copyright © 2007-2016, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -45,7 +45,7 @@ use Hoa\Socket;
  * Provide a DNS resolution server.
  * Please, see RFC6195, RFC1035 and RFC1034 for an overview.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 class Resolver implements Event\Listenable
