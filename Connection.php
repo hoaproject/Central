b310de34012b0af56a0e1ecc9c5fe0caed6d4c3c
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-01-09 09:01:49 +0100

    Quality: Run devtools:cs.

--- Connection.php
+++ Connection.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Hoa community. All rights reserved.
+ * Copyright © 2007-2016, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -44,7 +44,7 @@ namespace Hoa\Fastcgi;
  * http://fastcgi.com/devkit/doc/fcgi-spec.html.
  * Inspired by PHP SAPI code: php://sapi/cgi/fastcgi.*.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 abstract class Connection
@@ -130,7 +130,7 @@ abstract class Connection
      * @param   array  $pairs    Keys/values array.
      * @return  string
      */
-    public function packPairs(Array $pairs)
+    public function packPairs(array $pairs)
     {
         $out = null;
 
