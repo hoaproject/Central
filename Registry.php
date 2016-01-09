a6de1b926b6b2d7123e6c53a5ee5f65ee269306c
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-01-09 09:07:54 +0100

    Quality: Run devtools:cs.

--- Registry.php
+++ Registry.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Hoa community. All rights reserved.
+ * Copyright © 2007-2016, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -44,7 +44,7 @@ use Hoa\Protocol;
  *
  * Hold a register of something.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 class Registry extends \ArrayObject
@@ -156,7 +156,7 @@ class Registry extends \ArrayObject
  *
  * The `hoa://Library/Registry` node.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 class _Protocol extends Protocol\Node
