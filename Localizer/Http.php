e5dd3120bfbb5f18bb3ea8fccad2a6548204ba03
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-01-09 09:03:39 +0100

    Quality: Run devtools:cs.

--- Localizer/Http.php
+++ Localizer/Http.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Hoa community. All rights reserved.
+ * Copyright © 2007-2016, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -46,7 +46,7 @@ use Hoa\Http as HHttp;
  * Overview: http://www.w3.org/International/articles/language-tags/.
  * Specifications: RFC2822 and RFC3282.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 class Http implements Localizer
