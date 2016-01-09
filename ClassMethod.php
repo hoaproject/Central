51497bbaa83368c331314757f2368454484b5986
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-01-09 09:00:42 +0100

    Quality: Run devtools:cs.

--- ClassMethod.php
+++ ClassMethod.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Hoa community. All rights reserved.
+ * Copyright © 2007-2016, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -46,7 +46,7 @@ use Hoa\View;
  * This class dispatches on a class/object and a method, nothing more. There is
  * no concept of controller or action, it is just _call and _able.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 class ClassMethod extends Dispatcher
@@ -61,7 +61,7 @@ class ClassMethod extends Dispatcher
      * @throws  \Hoa\Dispatcher\Exception
      */
     protected function resolve(
-        Array $rule,
+        array $rule,
         Router $router,
         View\Viewable $view = null
     ) {
