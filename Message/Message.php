aa70d636b0683ced9eafb96c0265615d578414cd
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-01-09 09:12:01 +0100

    Quality: Run devtools:cs.

--- Message/Message.php
+++ Message/Message.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2015, Hoa community. All rights reserved.
+ * Copyright © 2007-2016, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -44,7 +44,7 @@ use Hoa\Consistency;
  * Write XML-RPC values intuitively on a message.
  * XML-RPC specification can be found at: http://xmlrpc.com/spec.
  *
- * @copyright  Copyright © 2007-2015 Hoa community
+ * @copyright  Copyright © 2007-2016 Hoa community
  * @license    New BSD License
  */
 class Message
