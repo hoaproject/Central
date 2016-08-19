f08811e910e51892d3d1c712868472171c453b4d
Pierre Tomasina <tomasina@plab.io>
2016-08-20 00:54:55 +0200

    add test case_insert_callable

--- Test/Unit/Max.php
+++ Test/Unit/Max.php
@@ -33,37 +33,77 @@ class Max extends Test\Unit\Suite
         $this
             ->given(
                 $max = new SUT(SUT::VALUE_TYPE_MIXED),
-                $concatKeys   = '',
-                $concatValues = ''
+                $keys   = '',
+                $series = '',
+                $four   = "ThisIsTheFourKey"
             )
             ->when(
-                $four  = $max->insert('4', 79),
+                $max->insert('4', 79, $four),
                 $three = $max->insert('3', 80),
                 $six   = $max->insert('6'),
                 $two   = $max->insert('2', 85),
                 $one   = $max->insert('1', 90),
-                $five  = $max->insert('5', 1)
+                $five  = $max->insert('5', 1),
+
+                $f = function() use ($max, & $series, & $keys) {
+
+                    foreach ($max as $key => $element) {
+
+                        $keys .= $key;
+                        $series .= $element;
+                    }
+                }
             )
-        ;
-
-        foreach ($max as $key => $element) {
-
-            $concatKeys .= $key;
-            $concatValues .= $element;
-        }
-
-        $this
-            ->then
-                ->string($concatKeys)
+            ->then($f())
+                ->string($keys)
                     ->isIdenticalTo($one . $two . $three . $four . $five . $six)
 
-                ->string($concatValues)
+                ->string($series)
                     ->isIdenticalTo('123456')
         ;
     }
 
+    /**
+     * @engine inline
+     */
     public function case_insert_callable()
     {
+        $this
+            ->given(
+                $max = new SUT(SUT::VALUE_TYPE_OBJECT),
+                $series = '',
+                $four   = "ThisIsTheFourKey"
+            )
+            ->when(
+                $max->insert(xcallable(function() use(& $series) { $series .= '4'; }), 79, $four),
+
+                $three = $max->insert(xcallable(function() use(& $series) { $series .= '3'; }), 80),
+                $six   = $max->insert(xcallable(function() use(& $series) { $series .= '6'; })),
+                $two   = $max->insert(xcallable(function() use(& $series) { $series .= '2'; }), 85),
+                $one   = $max->insert(xcallable(function() use(& $series) { $series .= '1'; }), 90),
+                $five  = $max->insert(xcallable(function() use(& $series) { $series .= '5'; }), 1),
+
+                $f = function() use ($max) {
+
+                    $keys = '';
+
+                    foreach ($max as $key => $element) {
+
+                        $keys .= $key;
+                        $element();
+                    }
+
+                    return $keys;
+                }
+            )
+            ->then
+                ->variable($keys = $f())
+                ->string($keys)
+                    ->isIdenticalTo($one . $two . $three . $four . $five . $six)
+
+                ->string($series)
+                    ->isIdenticalTo('123456')
+        ;
     }
 
     public function case_detach()
