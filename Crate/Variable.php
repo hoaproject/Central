e2797c6739811631343d43902987fec2c8b5ef0e
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-09 21:37:07 +0100

    Quality: Happy new year!

--- Crate/Variable.php
+++ Crate/Variable.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -44,7 +44,7 @@ use Hoa\Visitor;
  *
  * Represent a variable.
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 class Variable implements Realdom\IRealdom\Crate
@@ -62,7 +62,6 @@ class Variable implements Realdom\IRealdom\Crate
      * Constructor.
      *
      * @param   \Hoa\Realdom\IRealdom\Holder  $variable    Variable.
-     * @return  void
      */
     public function __construct(Realdom\IRealdom\Holder $variable)
     {
--- Realdom.php
+++ Realdom.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -46,7 +46,7 @@ use Hoa\Visitor;
  *
  * Abstract-top-super realistic domain :-).
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 abstract class Realdom
@@ -143,7 +143,6 @@ abstract class Realdom
     /**
      * Build a realistic domain.
      *
-     * @return  void
      * @throws  \Hoa\Realdom\Exception\IllegalArgument
      * @throws  \Hoa\Realdom\Exception\MissingArgument
      */
