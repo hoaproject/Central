1034856eb058c84b3b00467f028cf49013c01597
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-09 21:37:24 +0100

    Quality: Happy new year!

--- Grammar.pp
+++ Grammar.pp
@@ -6,7 +6,7 @@
 //
 // New BSD License
 //
-// Copyright © 2007-2016, Hoa community. All rights reserved.
+// Copyright © 2007-2017, Hoa community. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -35,7 +35,7 @@
 //
 // Provide grammar for JSON. Please, see <http://json.org>, RFC4627 or RFC7159.
 //
-// @copyright  Copyright © 2007-2016 Hoa community.
+// @copyright  Copyright © 2007-2017 Hoa community.
 // @license    New BSD License
 //
 
