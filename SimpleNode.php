1804913f5c04aae4866afff732bcca8bbb7ae0b2
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-09 21:38:09 +0100

    Quality: Happy new year!

--- SimpleNode.php
+++ SimpleNode.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -41,7 +41,7 @@ namespace Hoa\Graph;
  *
  * A very simple node.
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 class SimpleNode implements Node
@@ -67,7 +67,6 @@ class SimpleNode implements Node
      *
      * @param   string  $id       ID.
      * @param   string  $value    Value.
-     * @return  void
      */
     public function __construct($id, $value = null)
     {
