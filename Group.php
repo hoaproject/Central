4d6dba39b644cc86e996bad2ac5068b7cbf1a6c1
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-09 21:27:39 +0100

    Quality: Happy new year!

--- Group.php
+++ Group.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -47,7 +47,7 @@ use Hoa\Graph;
  * services cannot be inherited. If a group owns a service, this is a shared
  * service because several users can access to it.
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 class Group implements Graph\Node
