346da8f617a9b610bb4b3935f42e0b70c6e7d6d8
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-12 14:01:21 +0100

    Quality: Happy new year!

--- Shared.php
+++ Shared.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -43,7 +43,7 @@ use Hoa\Socket;
  *
  * Worker frontend, user's API.
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 class Shared
@@ -62,7 +62,6 @@ class Shared
      *
      * @param   mixed   $workerId    Worker ID or a socket client (i.e. a
      *                               \Hoa\Socket\Client object).
-     * @return  void
      * @throws  \Hoa\Worker\Exception
      */
     public function __construct($workerId)
